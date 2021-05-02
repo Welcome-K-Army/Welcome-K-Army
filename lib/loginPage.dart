12c33d45e3f721392458845a718cb9b5ceaddd25
FlutLab.io <>
2021-05-02 11:28:48 +0000

    Login Form(ver 1.0.0)

--- lib/loginPage.dart
+++ lib/loginPage.dart
@@ -7,6 +7,7 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
 
+    //final Size size=MediaQuery.of(context).size;
     return 
     Scaffold(
         body: Stack(
