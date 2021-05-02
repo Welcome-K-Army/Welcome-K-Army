045692d4ceb1a7d2c94bfeb7ada66b6493eae7cd
FlutLab.io <>
2021-05-02 08:23:08 +0000

    Autosave

--- lib/loginPage.dart
+++ lib/loginPage.dart
@@ -36,7 +36,7 @@ class LoginPage extends StatelessWidget {
                         ),
                         elevation:6,
                         child:Padding(
-                          padding: const EdgeInsets.only(left:12,right:12,top:12,bottom:32 ),
+                          padding: EdgeInsets.only(left:12,right:12,top:12,bottom:32 ),
                           child:Form(
                             key: _formkey,
                             child: Column(
