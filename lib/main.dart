ab75f3ebd95a4c02209682ebb6d7067c2004633b
FlutLab.io <>
2021-05-02 06:37:26 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -3,40 +3,57 @@ import 'package:flutter/material.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+  final GlobalKey<FormState> _formkey =GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
+    final Size size=MediaQuery.of(context).size;
+    final TextEditingController _emailController=TextEditingController();
+    final TextEditingController _passwordController=TextEditingController();
     return MaterialApp(
-      // Application name
-      title: 'Flutter Hello World',
-      // Application theme data, you can set the colors for the application as
-      // you want
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      // A widget which will be started on application startup
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatelessWidget {
-  final String title;
-
-  const MyHomePage({@required this.title});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        // The title text which will be shown on the action bar
-        title: Text(title),
-      ),
-      body: Center(
-        child: Text(
-          'Hello, World!',
-        ),
-      ),
-    );
+      home: Scaffold(
+        body: Stack(
+          alignment: Alignment.center,
+          children: <Widget>[
+            Container(
+              color: Colors.white,
+            ),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: <Widget>[
+                Container(
+                  width: 200,
+                  height: 200,
+                  color: Colors.blue,
+                ),
+                Stack(
+                  children: <Widget>[
+                    Form(
+                      key: _formkey,
+                      child:Column(
+                        children: <Widget>[
+                          TextFormField(),
+                          TextFormField(),
+                          Text("Forgot Password"),
+                        ],//<Widget>[]
+                      )),//Column,Form
+                    Container(
+                      width: 200,
+                      height: 200,
+                      color: Colors.black,
+                    ),
+                  ],
+                ), ////<Widget>[], Stack
+                Text("Don't Have an Account?"),
+                Container(
+                  height: size.height*0.05,
+                )
+              ], //<Widget>[]
+            ), //Column
+          ], //<Widget>[]
+        ), //Stack
+      ), //Scaffold
+    ); //MaterialApp
   }
 }
