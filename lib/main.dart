50b37d5592dc9de71348729e3759ae880d219704
FlutLab.io <>
2021-05-02 07:23:37 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -16,9 +16,7 @@ class MyApp extends StatelessWidget {
         body: Stack(
           alignment: Alignment.center,
           children: <Widget>[
-            Container(
-              color: Colors.white,
-            ),
+            Container(color: Colors.white,),
             Column(
               crossAxisAlignment: CrossAxisAlignment.center,
               mainAxisAlignment: MainAxisAlignment.end,
@@ -28,64 +26,59 @@ class MyApp extends StatelessWidget {
                   height: 200,
                   color: Colors.blue,
                 ),
-                Stack(
-                  children: <Widget>[
-                    Padding(
-                      padding: EdgeInsets.all(12),
-                      child: Card(
-                        shape: RoundedRectangleBorder(
-                          borderRadius:BorderRadius.circular(16)
-                        ),//RoundedRectangleBorder
-                        elevation:6,
-                        child: Padding(
-                          padding: const EdgeInsets.only(left:12,right:12,top:12,bottom:32),
-                          child:Form(
-                            key: _formkey,
-                            child:Column(
-                              crossAxisAlignment:CrossAxisAlignment.start,
-                              children: <Widget>[
-                                TextFormField(
-                                  controller: _emailController,
-                                  decoration: InputDecoration(
-                                    icon:Icon(Icons.account_circle),
-                                    labelText:"Email",
-                                  ),//InputDecoration
-                                validator:(String value){
+                Stack(children: <Widget>[
+                  Padding(
+                    padding: EdgeInsets.all(12),
+                    child: Card(
+                      shape: RoundedRectangleBorder(
+                        borderRadius:BorderRadius.circular(16)
+                      ),//RoundedRectangleBorder
+                      elevation:6,
+                      child: Padding(
+                        padding: const EdgeInsets.only(left:12,right:12,top:12,bottom:32),
+                        child:Form(
+                          key: _formkey,
+                          child:Column(
+                            crossAxisAlignment:CrossAxisAlignment.start,
+                            children: <Widget>[
+                              TextFormField(
+                                controller: _emailController,
+                                decoration: InputDecoration(
+                                  icon:Icon(Icons.account_circle),
+                                  labelText:"Email",
+                                ),//InputDecoration
+                              validator:(String value){
                                   if (value.isEmtpy){
                                     return "Please input correct Email";
                                   }
                                   return null;
                                 },
-                               ),
-                                TextFormField(
-                                  controller: _passwordController,
-                                  decoration: InputDecoration(
-                                    icon:Icon(Icons.vpn_key),
-                                    labelText:"Password",
-                                  ),//InputDecoration
-                                validator:(String value){
+                              ),//TextFormField
+                              TextFormField(
+                                controller: _passwordController,
+                                decoration: InputDecoration(
+                                  icon:Icon(Icons.vpn_key),
+                                  labelText:"Password",
+                                ),//InputDecoration
+                              validator:(String value){
                                   if (value.isEmtpy){
                                     return "Please input correct Password";
                                   }
                                   return null;
                                 },
-                               ),//TextFormField
-                                Text("Forgot Password"),
-                              ],//<Widget>[]
-                          )),//Column,Form
+                              ),//TextFormField
+                              Text("Forgot Password"),
+                            ],//<Widget>[]
+                          ),//Column
+                        ),//Form
                       ),//Padding
                     ),//Card
-                    Container(
-                      width: 200,
-                      height: 200,
-                      color: Colors.black,
-                    ),//Container 
                   ),//Padding
                 ],), //<Widget>[], Stack
                 Text("Don't Have an Account?"),
                 Container(
                   height: 20,
-                )
+                ),//Container
               ], //<Widget>[]
             ), //Column
           ], //<Widget>[]
