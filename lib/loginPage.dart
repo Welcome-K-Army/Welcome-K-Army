e591a5e14097ca1a8ffd16129e4236ecf81a3f0b
FlutLab.io <>
2021-05-02 10:56:35 +0000

    Autosave

--- lib/loginPage.dart
+++ lib/loginPage.dart
@@ -6,109 +6,98 @@ class LoginPage extends StatelessWidget {
   final TextEditingController _passwordController = TextEditingController();
   @override
   Widget build(BuildContext context) {
-
-    return 
-    Scaffold(
-        body: Stack(
-          alignment: Alignment.center,
-          children: <Widget>[
-            Container(
-              color: Colors.white,
-            ),
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: <Widget>[
-          //상단 이미지 부
-                Expanded(
-                  child:Padding(
-                    padding: const EdgeInsets.only(top: 40, left: 24, right: 24),
-                    child: FittedBox(
-                      fit: BoxFit.contain,
-                      child:CircleAvatar(
-                        backgroundImage : NetworkImage("https://lh3.googleusercontent.com/proxy/w1P2JMIYg4J5bSkZ594flSX5dvIxDkQ9rO8dgcGNzgOPxkOkJACiHXplqzmrVX584Uqnr6QX0IVqWK83DaB4LMsxzGNm_c8nKublP_uKmxgqmSPS9QZzs8z8-30uHa1i"),
-                      ),//CircleAvatar
-                    ),//FittedBox
-                  ),//Padding
-                ),//Expanded
-                // FadeInImage.assetNetwork(
-                //   placeholder:"image/Loading.gif",
-                //   image: "https://lh3.googleusercontent.com/proxy/w1P2JMIYg4J5bSkZ594flSX5dvIxDkQ9rO8dgcGNzgOPxkOkJACiHXplqzmrVX584Uqnr6QX0IVqWK83DaB4LMsxzGNm_c8nKublP_uKmxgqmSPS9QZzs8z8-30uHa1i",
-                // ),
-          //로그인 부
-                Stack(
-                  children: <Widget>[
-                    _inputForm(),
-                    _buttonLogin(),
-                  ],//<Widget>
-                ), //, Stack
-          //하단 회원가입 연결 부
-                Container(
-                  height: 40,
-                ),
-                Text("Don't Have an Account?"),
-                Container(
-                  height: 20,
-                )
-              ], //<Widget>[]
-            ), //Column
-          ], //<Widget>[]
-        ), //Stack
-      ); //Scaffold
+    return Scaffold(
+      body: Stack(
+        alignment: Alignment.center,
+        children: <Widget>[
+          Container(
+            color: Colors.white,
+          ),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: <Widget>[
+              //상단 이미지 부
+              Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.only(top: 40, left: 24, right: 24),
+                  child: FittedBox(
+                    fit: BoxFit.contain,
+                    child: CircleAvatar(
+                      backgroundImage: NetworkImage("https://lh3.googleusercontent.com/proxy/w1P2JMIYg4J5bSkZ594flSX5dvIxDkQ9rO8dgcGNzgOPxkOkJACiHXplqzmrVX584Uqnr6QX0IVqWK83DaB4LMsxzGNm_c8nKublP_uKmxgqmSPS9QZzs8z8-30uHa1i"),
+                    ), //CircleAvatar
+                  ), //FittedBox
+                ), //Padding
+              ), //Expanded
+              // FadeInImage.assetNetwork(
+              //   placeholder:"image/Loading.gif",
+              //   image: "https://lh3.googleusercontent.com/proxy/w1P2JMIYg4J5bSkZ594flSX5dvIxDkQ9rO8dgcGNzgOPxkOkJACiHXplqzmrVX584Uqnr6QX0IVqWK83DaB4LMsxzGNm_c8nKublP_uKmxgqmSPS9QZzs8z8-30uHa1i",
+              // ),
+              //로그인 부
+              Stack(
+                children: <Widget>[
+                  _inputForm(),
+                  _buttonLogin(),
+                ], //<Widget>
+              ), //, Stack
+              //하단 회원가입 연결 부
+              Container(
+                height: 40,
+              ),
+              Text("Don't Have an Account?"),
+              Container(
+                height: 20,
+              )
+            ], //<Widget>[]
+          ), //Column
+        ], //<Widget>[]
+      ), //Stack
+    ); //Scaffold
   }
 
-  Widget _inputForm(){
-    return  Padding(
+  Widget _inputForm() {
+    return Padding(
       padding: EdgeInsets.all(12),
-      child:Card(
-        shape:RoundedRectangleBorder(
-          borderRadius:BorderRadius.circular(16)
-        ),
-        elevation:6,
-        child:Padding(
-          padding: EdgeInsets.only(left:12,right:12,top:12,bottom:32 ),
-          child:Form(
-            key: _formkey,
-            child: Column(
-              crossAxisAlignment:CrossAxisAlignment.start,
-              children: <Widget>[
-              TextFormField(
-                controller: _emailController,
-                decoration: InputDecoration(icon:Icon(Icons.account_circle),
-                labelText:"Email",
-                ),
-              ),//TextFormField 이메일 바
-              TextFormField(
-                controller: _passwordController,
-                decoration: InputDecoration(icon:Icon(Icons.vpn_key),
-                labelText:"Password",
-                ),
-              ),//TextFormField 패스워드 바
-              Text("Forgot Password"),
-              ]//Widget
-            )//Column
-          ),//Form
-        ),//Padding
-      ),//Card
-    );//Padding
+      child: Card(
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(16)),
+        elevation: 6,
+        child: Padding(
+          padding: EdgeInsets.only(left: 12, right: 12, top: 12, bottom: 32),
+          child: Form(
+              key: _formkey,
+              child: Column(crossAxisAlignment: CrossAxisAlignment.start, children: <Widget>[
+                TextFormField(
+                  controller: _emailController,
+                  decoration: InputDecoration(
+                    icon: Icon(Icons.account_circle),
+                    labelText: "Email",
+                  ),
+                ), //TextFormField 이메일 바
+                TextFormField(
+                  controller: _passwordController,
+                  decoration: InputDecoration(
+                    icon: Icon(Icons.vpn_key),
+                    labelText: "Password",
+                  ),
+                ), //TextFormField 패스워드 바
+                Text("Forgot Password"),
+              ] //Widget
+                  ) //Column
+              ), //Form
+        ), //Padding
+      ), //Card
+    ); //Padding
   }
 
   Widget _buttonLogin() => Positioned(
-    left: 32,
-    right:32,
-    bottom:0,
-    Padding(
-      padding:EdgeInsets.symmetric(vertical: 20, )
-    child:RaisedButton(
-      child: Text("Login"),
-      color: Colors.green,
-      shape: RoundedRectangleBorder(
-        borderRadius:BorderRadius.circular(15)
-      ),
-      onPressed:(){
-
-      }
-    ),//RaisedButton
-    ),//Padding
-  );//Positioned
+        left: 32,
+        right: 32,
+        bottom: 0,
+        child: Padding(
+          padding: EdgeInsets.symmetric(
+            vertical: 20,
+          ),
+          child: RaisedButton(child: Text("Login"), color: Colors.green, shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)), onPressed: () {}), //RaisedButton
+        ), //Padding
+      ); //Positioned
 }
