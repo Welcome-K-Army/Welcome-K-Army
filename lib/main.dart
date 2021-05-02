39dff89098d7aadfd2cec007078e51ba50e48e18
FlutLab.io <>
2021-05-02 07:07:03 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -30,22 +30,58 @@ class MyApp extends StatelessWidget {
                 ),
                 Stack(
                   children: <Widget>[
-                    Form(
-                      key: _formkey,
-                      child:Column(
-                        children: <Widget>[
-                          TextFormField(),
-                          TextFormField(),
-                          Text("Forgot Password"),
-                        ],//<Widget>[]
-                      )),//Column,Form
+                    Padding(
+                      padding: EdgeInsets.all(12),
+                      child: Card(
+                        shape: RoundedRectangleBorder(
+                          borderRadius:BorderRadius.circular(16)
+                        ),//RoundedRectangleBorder
+                        elevation:6,
+                        child: Padding(
+                          padding: const EdgeInsets.only(left:12,right:12,top:12,bottom:32),
+                          child:Form(
+                            key: _formkey,
+                            child:Column(
+                              crossAxisAlignment:CrossAxisAlignment.start,
+                              children: <Widget>[
+                                TextFormField(
+                                  controller: _emailController,
+                                  decoration: InputDecoration(
+                                    icon:Icon(Icons.account_circle),
+                                    labelText:"Email",
+                                  ),//InputDecoration
+                                validator:(String value){
+                                  if (value.isEmtpy){
+                                    return "Please input correct Email";
+                                  }
+                                  return null;
+                                },
+                               ),
+                                TextFormField(
+                                  controller: _passwordController,
+                                  decoration: InputDecoration(
+                                    icon:Icon(Icons.vpn_key),
+                                    labelText:"Password",
+                                  ),//InputDecoration
+                                validator:(String value){
+                                  if (value.isEmtpy){
+                                    return "Please input correct Password";
+                                  }
+                                  return null;
+                                },
+                               ),//TextFormField
+                                Text("Forgot Password"),
+                              ],//<Widget>[]
+                          )),//Column,Form
+                      ),//Padding
+                    ),//Card
                     Container(
                       width: 200,
                       height: 200,
                       color: Colors.black,
-                    ),
-                  ],
-                ), ////<Widget>[], Stack
+                    ),//Container 
+                  ),//Padding
+                ],), //<Widget>[], Stack
                 Text("Don't Have an Account?"),
                 Container(
                   height: 20,
