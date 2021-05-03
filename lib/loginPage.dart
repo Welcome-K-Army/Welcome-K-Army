5d3db950e3b0df631657501b8a9cd6a00f2692f6
FlutLab.io <>
2021-05-03 10:49:55 +0000

    Autosave

--- lib/loginPage.dart
+++ lib/loginPage.dart
@@ -5,8 +5,9 @@ import 'joinOrLogin.dart';
 
 class LoginPage extends StatelessWidget {
   final GlobalKey<FormState> _formkey = GlobalKey<FormState>();
-  final TextEditingController _emailController = TextEditingController();
-  final TextEditingController _passwordController = TextEditingController();
+  final TextEditingController _emailController = TextEditingController();//email 컨트롤러
+  final TextEditingController _passwordController = TextEditingController();//password 컨트롤러
+  
   @override
   Widget build(BuildContext context) {
 
@@ -146,7 +147,7 @@ Widget get _LogoImage => Expanded(
     child: FittedBox(
       fit: BoxFit.contain,
       child:CircleAvatar(
-        backgroundImage : NetworkImage("https://lh3.googleusercontent.com/proxy/w1P2JMIYg4J5bSkZ594flSX5dvIxDkQ9rO8dgcGNzgOPxkOkJACiHXplqzmrVX584Uqnr6QX0IVqWK83DaB4LMsxzGNm_c8nKublP_uKmxgqmSPS9QZzs8z8-30uHa1i"),
+        backgroundImage : NetworkImage("https://previews.123rf.com/images/phanuchat/phanuchat1604/phanuchat160400014/55601666-%EA%B5%B0%EC%82%AC-%EB%82%A8%EC%84%B1%EA%B3%BC-%EC%97%AC%EC%84%B1-%ED%9D%B0%EC%83%89-%EB%B0%B0%EA%B2%BD-%EA%B7%80%EC%97%AC%EC%9A%B4-%ED%94%8C%EB%9E%AB-%EB%A7%8C%ED%99%94-%EC%8A%A4%ED%83%80%EC%9D%BC-%EA%B2%A9%EB%A6%AC-%EB%90%9C-%EB%B2%A1%ED%84%B0-%EC%9D%BC%EB%9F%AC%EC%8A%A4%ED%8A%B8-%EB%A0%88%EC%9D%B4-%EC%85%98%EC%97%90-%EA%B3%A0%EB%A6%BD-%EB%90%9C-%ED%8F%89%EB%A9%B4-%EB%A7%8C%ED%99%94-%EC%BA%90%EB%A6%AD%ED%84%B0-%EB%94%94%EC%9E%90%EC%9D%B8%EC%97%90-%EC%9C%84%EC%9E%A5-%EC%A0%9C%EB%B3%B5%EC%9D%84-%EC%9E%85%EC%9D%80-%EA%B5%B0%EC%9D%B8.jpg"),
       ),//CircleAvatar
     ),//FittedBox
   ),//Padding
