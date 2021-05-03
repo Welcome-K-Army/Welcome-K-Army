160c51460533e4b48a052dc27ded76e923a81163
FlutLab.io <>
2021-05-03 14:07:26 +0000

    Autosave

--- lib/Home.dart
+++ lib/Home.dart
@@ -2,23 +2,23 @@ import 'package:flutter/foundation.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
 class Mainpage extends StatelessWidget {
-
   Mainpage({this.email});
 
   final String email;
-  
+
   @override
   Widget build(BuildContext context) {
     return Container(
       appBar: AppBar(
-        title:Text(email),
+        title: Text(email),
       ),
       body: Container(
-        child:Center(
-          child:FlatButton(onPressed: (){
-            FirebaseAuth.instance.signOut();
-          }, child: Text("Logout")
-          ),
+        child: Center(
+          child: FlatButton(
+              onPressed: () {
+                FirebaseAuth.instance.signOut();
+              },
+              child: Text("Logout")),
         ),
       ),
     );
