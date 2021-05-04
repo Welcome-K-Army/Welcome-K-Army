757f27658da3e7ce288a8ba9b84cf7975469b48e
FlutLab.io <>
2021-05-04 13:52:54 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'loginPage.dart';
 import 'joinOrLogin.dart';
 import 'Home.dart';
+
 void main() async{
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
