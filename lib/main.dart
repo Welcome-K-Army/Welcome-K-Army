6666c290a9fd0534b2ae3bacd7d8f0414889c0fc
FlutLab.io <>
2021-05-04 03:30:47 +0000

    Split mainView from main

--- lib/main.dart
+++ lib/main.dart
@@ -1,80 +1,18 @@
-import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/basic.dart';
-
-void main() => runApp(new MyApp());
-
-class MyApp extends StatefulWidget {
-  MyAppState createState() => new MyAppState();
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/basic.dart';
+import 'mainView/mainView.dart';
+void main() => runApp(new MyApp());
+// bottomNavigationBar는 항상 내용 재구성 해야되서 Stateful로 구성
+class MyApp extends StatefulWidget {
+  MyAppState createState() => new MyAppState();
 }
-
-class MyAppState extends State<MyApp> {
-  bool checkBoxValue = false;
-  String actionText = "Default";
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: "Welcome K-Army",
-      theme: ThemeData(primaryColor: Colors.blue),
-      home: DefaultTabController(
-          length: 3,
-          child: Scaffold(
-            appBar: AppBar(
-              title: Text('welcome k-army'),
-            ),
-            body: TabBarView(
-              children: [
-                Text('홈 스크린'),
-                Text('검색 스크린'),
-                Text('마이 스크린'),
-              ],
-            ),
-            bottomNavigationBar: BottomBar(),
-          )),
-    );
-  }
-}
-
-class BottomBar extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      color: Colors.yellow,
-      child: Container(
-        height: 70,
-        padding: EdgeInsets.only(bottom: 10, top: 5),
-        child: TabBar(
-          indicatorSize: TabBarIndicatorSize.label,
-          indicatorColor: Colors.red,
-          indicatorWeight: 4,
-          labelColor: Colors.white,
-          unselectedLabelColor: Colors.black38,
-          labelStyle: TextStyle(
-            fontSize: 14, /*fontFamilyFallback: fontFamilyName('Rubik')*/
-          ),
-          tabs: [
-            Tab(
-              icon: Icon(
-                Icons.home,
-                size: 20,
-              ),
-              text: 'Home',
-            ),
-            Tab(
-              icon: Icon(
-                Icons.search,
-                size: 20,
-              ),
-              text: 'Search',
-            ),
-            Tab(
-                icon: Icon(
-                  Icons.people,
-                  size: 20,
-                ),
-                text: 'My')
-          ],
-        ),
-      ),
-    );
-  }
+class MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Welcome K-Army",
+      theme: ThemeData(primaryColor: Colors.blue),
+      home: MainView(),
+    );
+  }
 }
