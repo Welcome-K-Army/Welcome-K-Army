5f8fcd80f8e4ae2851acc5f8fad34a1f039a1a13
FlutLab.io <>
2021-05-04 04:01:08 +0000

    split mainView from mainV

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,20 @@
-import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/basic.dart';
-import 'mainView/mainView.dart';
-void main() => runApp(new MyApp());
-// bottomNavigationBar는 항상 내용 재구성 해야되서 Stateful로 구성
-class MyApp extends StatefulWidget {
-  MyAppState createState() => new MyAppState();
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/basic.dart';
+import 'mainView/mainView.dart';
+
+void main() => runApp(new MyApp());
+
+class MyApp extends StatefulWidget {
+  MyAppState createState() => new MyAppState();
 }
-class MyAppState extends State<MyApp> {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: "Welcome K-Army",
-      theme: ThemeData(primaryColor: Colors.blue),
-      home: MainView(),
-    );
-  }
+
+class MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'welcome k-army',
+      theme: ThemeDate(primarycolor: Colors.blue),
+      home: MainView(),
+    );
+  }
 }
