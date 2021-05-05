0de29a935eaa916215afe0b9c07b489ed70fd6f8
FlutLab.io <>
2021-05-05 07:19:22 +0000

    Autosave

--- lib/homeView/noticeView/noticeView.dart
+++ lib/homeView/noticeView/noticeView.dart
@@ -4,9 +4,9 @@ import 'package:flutter/src/widgets/basic.dart';
 import 'notice.dart';
 
 final List<String> noticeList = [
-  new Notice(title: "Intro", context: 'Hello World'),
-  new Notice(title: 'App Info', context: 'Welcome K Army'),
-  new Notice(title: 'Outtro', context: 'Good Bye'),
+  new Notice(title: "Intro", context: 'Hello World');
+  new Notice(title: 'App Info', context: 'Welcome K Army');
+  new Notice(title: 'Outtro', context: 'Good Bye');
 ];
 
 class NoticeView extends StatefulWidget {
