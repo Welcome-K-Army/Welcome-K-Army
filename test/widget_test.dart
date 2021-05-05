88b549bc6cc52f0a78ffd112bedd15b8e49d56a9
FlutLab.io <>
2021-05-05 04:05:22 +0000

    Autosave

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:welcomkarmylogin/main.dart';
+import 'package:welcomekarmylogin/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
