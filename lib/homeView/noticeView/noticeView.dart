196126fdd759adc4e8208f4fbd91748a724ba2cf
FlutLab.io <>
2021-05-05 06:55:21 +0000

    Autosave

--- lib/homeView/noticeView/noticeView.dart
+++ lib/homeView/noticeView/noticeView.dart
@@ -1,49 +1,53 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/basic.dart';
 
-class Notice {
-  var date = DateTime().now;
-  String title;
-  String context;
+import 'notice.dart';
 
-  Notice(this.title, this.context);
+final List<String> noticeList = [
+  Notice(title: 'Intro', context: 'Hello World'),
+  Notice(title: 'App Info', context: 'Welcome K Army'),
+  Notice(title: 'Outtro', context: 'Good Bye'),
+];
+
+class NoticeView extends StatefulWidget {
+  NoticeViewState createState() => new NoticeViewState();
 }
 
-class NoticeTile {
-  NoticeTile(this._notice);
-
-  final Notice _notice;
-
-  @override
-  Widget build(BuildContext context) {
-    return ListTile(
-        title: Column(
-      children: <Widget>[
-        Text(_notice.title),
-        Text(_notice.context),
-        Text(_notice.date),
-      ], // ListTile Column children
-    ));
-  }
-}
-
-class HeaderTile {
+class NoticeViewState extends State<NoticeView> {
   @override
   Widget build(BuildContext context) {
     return Container(
-        child: SizedBox(
-            width: 36.0,
-            height: 25.0,
-            child: ClipRRect(
-                borderRadius: BorderRadius.circula(36.0),
-                child: Container(
-                    decoration: BoxDecoration(color: Colors.white, boxShadow: [
-                  BoxShadow(color: Color.black12, offset: Offset(3.0, 6.0), blurRadius: 10.0),
-                ] // boxShadw
-                        ) // BoxDecoration
-                    ) // Container
-                ) // ClipRRect
-            ) // SizedBox
-        ); // Return Container
+      color: Color(0xFFEDF0F4),
+      height: 280,
+      child: Padding(
+        padding: EdgeInsets.all(10),
+        child: Column(
+          children: <Widget>[
+            Row(
+              children: <Widget>[
+                Padding(
+                  padding: EdgeInsets.only(right: 5, bottom: 5),
+                  child: Icon(Icons.star, size: 20),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(bottom: 5),
+                  child: Text("Notice"),
+                ),
+              ], // Row children
+            ), // Row
+            ListView.separated(
+                itemCount: noticeList.lenght,
+                itemBuilder: (context, index) {
+                  if (index == 0) return HeaderTile();
+                  return NoticeTile(noticeList[length - 1]);
+                },
+                separatorBuilder: (context, index) {
+                  if (index == 0) return SizedBox.shrink();
+                  return const Divider();
+                }) // ListView
+          ], // Column children
+        ), // Column
+      ), // Padding
+    ); // Container
   }
 }
