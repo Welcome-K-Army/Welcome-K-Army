a923f33680705e8caa831218d5f2520022260525
1kl1 <mamy0320@naver.com>
2021-05-05 22:40:28 +0900

    map api first commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,59 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/basic.dart';
-import 'mainView/mainView.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
-void main() => runApp(new MyApp());
+void main() => runApp(MyApp());
 
-//bottomNavigationBar는 항상 내용 재구성 해야되서 Stateful로 구성
-class MyApp extends StatefulWidget {
-  MyAppState createState() => new MyAppState();
-}
-
-//Bottom Tab으로 구성된 MainView, View 추가시 length 값 변경 및 body의 TabBarView에 생성자 추가할 것
-class MyAppState extends State<MyApp> {
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'welcome k army',
-      theme: ThemeData(primaryColor: Colors.blue),
-      home: MainView(),
+      title: 'Flutter Google Maps Demo',
+      home: MapSample(),
     );
+  }
+}
+
+class MapSample extends StatefulWidget {
+  @override
+  State<MapSample> createState() => MapSampleState();
+}
+
+class MapSampleState extends State<MapSample> {
+  Completer<GoogleMapController> _controller = Completer();
+
+  static final CameraPosition _kGooglePlex = CameraPosition(
+    target: LatLng(37.42796133580664, -122.085749655962),
+    zoom: 14.4746,
+  );
+
+  static final CameraPosition _kLake = CameraPosition(
+      bearing: 192.8334901395799,
+      target: LatLng(37.43296265331129, -122.08832357078792),
+      tilt: 59.440717697143555,
+      zoom: 19.151926040649414);
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      body: GoogleMap(
+        mapType: MapType.hybrid,
+        initialCameraPosition: _kGooglePlex,
+        onMapCreated: (GoogleMapController controller) {
+          _controller.complete(controller);
+        },
+      ),
+      floatingActionButton: FloatingActionButton.extended(
+        onPressed: _goToTheLake,
+        label: Text('To the lake!'),
+        icon: Icon(Icons.directions_boat),
+      ),
+    );
+  }
+
+  Future<void> _goToTheLake() async {
+    final GoogleMapController controller = await _controller.future;
+    controller.animateCamera(CameraUpdate.newCameraPosition(_kLake));
   }
 }
