3fd5ad63d302f79279025a137540e984359e662f
FlutLab.io <>
2021-05-05 03:39:01 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -17,7 +17,10 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Splash(),
+      home: ChangeNotifierProvider<JoinOrLogin>.value(
+          value: JoinOrLogin(), 
+          child: LoginPage())//ChangeNotifierProvider
+          //Splash(),
     ); //MaterialApp
   }
 }
