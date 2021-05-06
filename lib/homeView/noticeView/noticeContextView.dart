d41fb448352836010be744e77de74f382733abea
FlutLab.io <>
2021-05-06 13:58:12 +0000

    Autosave

--- lib/homeView/noticeView/noticeContextView.dart
+++ lib/homeView/noticeView/noticeContextView.dart
@@ -4,28 +4,43 @@ import 'package:flutter/src/widgets/basic.dart';
 import 'notice.dart';
 
 class NoticeContextView extends StatefulWidget {
-  NoticeContextViewState createState() => new NoticeContextViewState();
+  int noticeNumber;
+  NoticeContextViewState createState() => new NoticeContextViewState(noticeNumber));
 }
 
 class NoticeContextViewState extends State<NoticeContextView> {
+  int noticeNumber;
+  NoticeContextViewState(this.noticeNumber);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
-      body: ListView(
+      body: ListView.separated(
         children: 
           <Widget> [
           Row(children: <Widget>[
-            Text(noticeList.number),
-            Text(noticeList.title),
-            Text(noticeList.date_yMMMd),
+            Text(noticeList[noticeNumber].number),
+            Text(noticeList[noticeNumber].title),
+            Text(noticeList[noticeNumber].date_yMMMd),
           ]), // Row
           Column(children: <Widget>[
-            Text(noticeList.context),
-            Text(noticeList.person),
+            Text(noticeList[noticeNumber].context),
+            Text(noticeList[noticeNumber].person),
           ]), // Column
           ]
       ), // Container
     ); // Scaffold
   }
 }
+ListView.separated(
+                shrinkWrap: true,
+                itemCount: noticeList.length+1,
+                itemBuilder: (context, index) {
+                  if (index == 0) return HeaderTile();
+                  return NoticeTile(noticeList[index - 1]);
+                },
+                separatorBuilder: (context, index) {
+                  if (index == 0) return SizedBox.shrink();
+                  return const Divider();
+                }) // ListView
