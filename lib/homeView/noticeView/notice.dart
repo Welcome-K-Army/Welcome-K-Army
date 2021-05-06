60ba9029965f1821b1b1c0fab3886678259443a6
FlutLab.io <>
2021-05-06 03:40:57 +0000

    Autosave

--- lib/homeView/noticeView/notice.dart
+++ lib/homeView/noticeView/notice.dart
@@ -37,7 +37,7 @@ class NoticeTile extends StatelessWidget {
         // 공지 제목을 누르면 공지 게시글로 이동
         Navigator.push(
           context,
-          MaterialPageRoute(builder: (context) => noticeContextView()),
+          MaterialPageRoute(builder: (context) => NoticeContextView()),
         );
       },
     ); // ListTile
