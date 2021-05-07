1782258b68e44c1844ef67006ac8cc2ec4f032e0
FlutLab.io <>
2021-05-07 10:37:37 +0000

    Autosave

--- lib/loginPage.dart
+++ lib/loginPage.dart
@@ -68,60 +68,60 @@ class LoginPage extends StatelessWidget {
         child: Padding(
           padding: EdgeInsets.only(left: 12, right: 12, top: 12, bottom: 32),
           child: Form(
-              key: _formkey,
-              child: SingleChildScrollView(
-              Column(crossAxisAlignment: CrossAxisAlignment.start, children: <Widget>[
-                TextFormField(
-                    //이메일
-                    controller: _emailController,
-                    decoration: InputDecoration(
-                      icon: Icon(Icons.account_circle),
-                      labelText: "Email",
-                    ), //InputDecoration
-                    validator: (String value) {
-                      if (value.isEmpty) {
-                        return "Please input correct Email!";
-                      } else if (!RegExp(r"[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?").hasMatch(value.toString())) {
-                        //이메일 정규 표현식
-                        return "Not correct Email format";
-                      }
+            key: _formkey,
+            child: SingleChildScrollView(
+              child:Column(crossAxisAlignment: CrossAxisAlignment.start, children: <Widget>[
+              TextFormField(
+                  //이메일
+                  controller: _emailController,
+                  decoration: InputDecoration(
+                    icon: Icon(Icons.account_circle),
+                    labelText: "Email",
+                  ), //InputDecoration
+                  validator: (String value) {
+                    if (value.isEmpty) {
+                      return "Please input correct Email!";
+                    } else if (!RegExp(r"[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?").hasMatch(value.toString())) {
+                      //이메일 정규 표현식
+                      return "Not correct Email format";
+                    }
 
-                      return null;
-                    }), //TextFormField 이메일
-                TextFormField(
-                    //패스워드
-                    obscureText: true,
-                    controller: _passwordController,
-                    decoration: InputDecoration(
-                      icon: Icon(Icons.vpn_key),
-                      labelText: "Password",
-                    ), //InputDecoration
-                    validator: (String value) {
-                      if (value.isEmpty) {
-                        return "Please input correct Password!";
-                      } else if (value.length < 8) {
-                        return "Please enter a password of at least 8 digits!";
-                      }
-                      return null;
-                    }), //TextFormField 패스워드
-                Container(
-                  height: 10,
-                ),
-                Consumer<JoinOrLogin>(
-                  builder: (context, value, child) => Opacity(
-                      opacity: value.isJoin ? 0 : 1,
-                      child: GestureDetector(
-                          onTap: value.isJoin
-                              ? null
-                              : () {
-                                  goToForgetPw(context);
-                                },
-                          child: Text("Forgot Password"))),
-                ),
-              ] //Widget
-                  ) //Column
-              ),//SingleChildScrollView
-              ), //Form
+                    return null;
+                  }), //TextFormField 이메일
+              TextFormField(
+                  //패스워드
+                  obscureText: true,
+                  controller: _passwordController,
+                  decoration: InputDecoration(
+                    icon: Icon(Icons.vpn_key),
+                    labelText: "Password",
+                  ), //InputDecoration
+                  validator: (String value) {
+                    if (value.isEmpty) {
+                      return "Please input correct Password!";
+                    } else if (value.length < 8) {
+                      return "Please enter a password of at least 8 digits!";
+                    }
+                    return null;
+                  }), //TextFormField 패스워드
+              Container(
+                height: 10,
+              ),
+              Consumer<JoinOrLogin>(
+                builder: (context, value, child) => Opacity(
+                    opacity: value.isJoin ? 0 : 1,
+                    child: GestureDetector(
+                        onTap: value.isJoin
+                            ? null
+                            : () {
+                                goToForgetPw(context);
+                              },
+                        child: Text("Forgot Password"))),
+              ),
+            ] //Widget
+                    ) //Column
+                ), //SingleChildScrollView
+          ), //Form
         ), //Padding
       ), //Card
     ); //Padding
