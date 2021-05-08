5d3b28a0d5f4461e6748749381f1e26bf2f2c6e6
FlutLab.io <>
2021-05-08 06:28:55 +0000

    Autosave

--- lib/homeView/menuView/menuView.dart
+++ lib/homeView/menuView/menuView.dart
@@ -11,12 +11,14 @@ class MenuViewState extends State<MenuView> {
   @override
   Widget build(BuildContext context) {
     return GridView.builder(
-      itemCount: menuList.length,
+      shrinkWrap: true,
+      physics: ClampingScrollPhysics(), 
       gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
         crossAxisCount: 3,
         crossAxisSpacing: 2,
         mainAxisSpacing: 2,
       ),
+      itemCount: menuList.length,
       itemBuilder: (context, index) {
         return MenuIconButton(menuList[index]);
       },
