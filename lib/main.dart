37c45aab4e86735cf2ec4e89bca4b5c44f4d39fd
FlutLab.io <>
2021-05-08 06:53:50 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -5,18 +5,17 @@ import 'mainView/mainView.dart';
 void main() => runApp(new MyApp());
 
 //bottomNavigationBar는 항상 내용 재구성 해야되서 Stateful로 구성
-class MyApp extends StatefulWidget {
-  MyAppState createState() => new MyAppState();
-}
-
-//Bottom Tab으로 구성된 MainView, View 추가시 length 값 변경 및 body의 TabBarView에 생성자 추가할 것
-class MyAppState extends State<MyApp> {
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'welcome k army',
-      theme: ThemeData(primaryColor: Colors.blue),
+      theme: ThemeData(
+        primaryColor: kPrimaryColor,
+        textTheme: Theme.of(context).textTheme.apply(bodyColor: kTextColor),
+        visualDensity: VisualDensity.adaptivePlatformDensity(),
+      ),
       home: MainView(),
-    );
+    )
   }
 }
