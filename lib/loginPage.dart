9b2968ac106b40fa5a730d0d1eaeb41302b579b9
FlutLab.io <>
2021-05-08 05:14:47 +0000

    Autosave

--- lib/loginPage.dart
+++ lib/loginPage.dart
@@ -181,6 +181,8 @@ class _LoginPage extends State<LoginPage> {
             }), //TextFormField 이메일
         Row(
           children: <Widget>[
+            Icon(Icons.Wc)
+            ,
             Flexible(
               fit: FlexFit.loose,
               child: RadioListTile<Gender>(
