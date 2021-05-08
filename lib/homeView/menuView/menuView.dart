0bf285729f128894c2ea94bdf3e10fe7e4f231db
FlutLab.io <>
2021-05-08 08:11:12 +0000

    Autosave

--- lib/homeView/menuView/menuView.dart
+++ lib/homeView/menuView/menuView.dart
@@ -10,32 +10,94 @@ class MenuView extends StatefulWidget {
 class MenuViewState extends State<MenuView> {
   @override
   Widget build(BuildContext context) {
-    return Card(
+    return Container(
         child: Padding(
-          padding: EdgeInsets.all(10),
-          child: Column(
-          children: <Widget>[
-            Row(
-              children: <Widget> [
-                Icon(Icons.star, size: 20),
-                Text('자주 찾는 메뉴'),
-                Icon(Icons.add, size: 20),
-              ]
-            ),
-            GridView.builder(
-              shrinkWrap: true,
-              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                crossAxisCount: 3,
-                crossAxisSpacing: 2,
-                mainAxisSpacing: 2,
-              ),
-              itemCount: menuList.length,
-              itemBuilder: (context, index) {
-                return MenuIconButton(menuList[index]);
-              },
-            )
-          ]
-      ))
-    ); // GridView
+            padding: EdgeInsets.all(10),
+            child: Column(children: <Widget>[
+              MenuTitleWithMoreBtn(title: "자주찾는메뉴", press: () {}),
+              GridView.builder(
+                shrinkWrap: true,
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 3,
+                  crossAxisSpacing: 2,
+                  mainAxisSpacing: 2,
+                ),
+                itemCount: menuList.length,
+                itemBuilder: (context, index) {
+                  return MenuIconButton(menuList[index]);
+                },
+              )
+            ]))); // GridView
   } // Widget
 } // Class
+
+class MenuTitleWithCustomUnderLine extends StatelessWidget {
+  const MenuTitleWithCustomUnderLine({
+    Key key,
+    this.text,
+  }) : super(key: key);
+
+  final String text;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 24,
+      child: Stack(
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.only(left: kDefaultPadding / 4),
+            child: Text(
+              text,
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+            ), // Text
+          ), // Padding
+          Positioned(
+            bottom: 0,
+            left: 0,
+            right: 0,
+            child: Container(
+              margin: EdgeInsets.only(right: kDefaultPadding / 4),
+              height: 7,
+              color: kPrimaryColor.withOpacity(0.2),
+            ), // Container
+          ), // Positioned
+        ], // <Widget> []
+      ), // Stack
+    ); // Container
+  }
+}
+
+class MenuTitleWithMoreBtn extends StatelessWidget {
+  const MenuTitleWithMoreBtn({
+    Key key,
+    this.text,
+  }) : super(key: key);
+
+  final String title;
+  final Function press;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: <Widget>[
+        MenuTitleWithCustomUnderLine(text: title),
+        Spacer(),
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: kDefaultPadding),
+          child: FlatButton(
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(20),
+            ), // RoundedRectangleBorder
+            color: kPrimaryColor,
+            onPressed: press,
+            child: Text(
+              "더보기",
+              style: TextStyle(color: Colors.white),
+            ), // Text
+          ), // FlatButton
+        ),
+      ], // <Widget>[]
+    ); // Row
+  }
+}
