c5ccbaa801c3e98b001534de70d52526306a181f
FlutLab.io <>
2021-05-08 06:37:31 +0000

    Autosave

--- lib/homeView/menuView/menuView.dart
+++ lib/homeView/menuView/menuView.dart
@@ -10,17 +10,30 @@ class MenuView extends StatefulWidget {
 class MenuViewState extends State<MenuView> {
   @override
   Widget build(BuildContext context) {
-    return GridView.builder(
-      shrinkWrap: true,
-      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-        crossAxisCount: 3,
-        crossAxisSpacing: 2,
-        mainAxisSpacing: 2,
-      ),
-      itemCount: menuList.length,
-      itemBuilder: (context, index) {
-        return MenuIconButton(menuList[index]);
-      },
+    return Card(
+        child: Column(
+          children:: <Widget>[
+            Row(
+              children: <Widget> [
+                Icon(Icons.star, size: 5),
+                Text('자주 찾는 메뉴'),
+                Icon(Icons.plus, size: 5),
+              ]
+            ),
+            GridView.builder(
+              shrinkWrap: true,
+              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 3,
+                crossAxisSpacing: 2,
+                mainAxisSpacing: 2,
+              ),
+              itemCount: menuList.length,
+              itemBuilder: (context, index) {
+                return MenuIconButton(menuList[index]);
+              },
+            )
+          ]
+      )
     ); // GridView
   } // Widget
 } // Class
