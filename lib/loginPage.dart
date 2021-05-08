e7340f93692e2d2bdabdc43263cb1d4c65a1a1e7
FlutLab.io <>
2021-05-08 02:19:08 +0000

    Autosave

--- lib/loginPage.dart
+++ lib/loginPage.dart
@@ -151,7 +151,7 @@ class LoginPage extends StatelessWidget {
             validator: (String value) {
               if (value.isEmpty) {
                 return "Please input correct NickName!";
-              } else if ()
+              } else if (){
                 return "Not correct NickName format";
               }
               return null;
