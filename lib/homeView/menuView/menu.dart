4886d119d5064715bfa3253958d540987842e9a1
FlutLab.io <>
2021-05-08 06:16:45 +0000

    Autosave

--- lib/homeView/menuView/menu.dart
+++ lib/homeView/menuView/menu.dart
@@ -2,10 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/basic.dart';
 
 final List<Menu> menuList = [
-  new Menu(name: '공지사항', icon: Icon(Icons.star. size: 10),
-  new Menu(name: '지도검색', icon: Icon(Icons.location_pin. size: 10),
-  new Menu(name: '입시정보', icon: Icon(Icons.search. size: 10),
-  new Menu(name: '일정', icon: Icon(Icons.calendar_today. size: 10),
+  new Menu(name: '공지사항', icon: Icon(Icons.star. size: 10)),
+  new Menu(name: '지도검색', icon: Icon(Icons.location_pin. size: 10)),
+  new Menu(name: '입시정보', icon: Icon(Icons.search. size: 10)),
+  new Menu(name: '일정', icon: Icon(Icons.calendar_today. size: 10)),
 ];
 
 class Menu {
@@ -25,7 +25,7 @@ class MenuIconButton extends StatelessWidget {
     return InkWell(
       onTap: () {},
       child: Column(mainAxisAlignment: MainAxisAlignment.center, children: <Widget>[
-        Icon(_Menu.icon),
+        _Menu.icon,
         Text(_Menu.name),
       ]), // Column
     ); // ListTile
