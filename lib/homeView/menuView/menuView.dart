3239aabb3eebac44990588063a1bc8098d9fd920
FlutLab.io <>
2021-05-09 06:16:31 +0000

    Autosave

--- lib/homeView/menuView/menuView.dart
+++ lib/homeView/menuView/menuView.dart
@@ -19,22 +19,21 @@ class MenuViewState extends State<MenuView> {
             child: Column(children: <Widget>[
               MenuTitleWithMoreBtn(title: "Favorite", press: () {}),
               Card(
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(15.0),
-                ),
-                child: GridView.builder(
-                shrinkWrap: true,
-                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                  crossAxisCount: 4,
-                  crossAxisSpacing: 2,
-                  mainAxisSpacing: 2,
-                ),
-                itemCount: menuList.length,
-                itemBuilder: (context, index) {
-                  return MenuIconButton(menuList[index]);
-                },
-              )
-              ),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(15.0),
+                  ),
+                  child: GridView.builder(
+                    shrinkWrap: true,
+                    gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 4,
+                      crossAxisSpacing: 3,
+                      mainAxisSpacing: 3,
+                    ),
+                    itemCount: menuList.length,
+                    itemBuilder: (context, index) {
+                      return MenuIconButton(menuList[index]);
+                    },
+                  )),
             ]))); // GridView
   } // Widget
 } // Class
