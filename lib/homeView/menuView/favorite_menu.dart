14bbe134719b764c0c4decec0deadddc2540d443
FlutLab.io <>
2021-05-09 06:21:13 +0000

    Autosave

---
+++ lib/homeView/menuView/favorite_menu.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/basic.dart';
+
+import '../../constants.dart';
+import 'menu.dart';
+
+class FavoriteMenu extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(15.0),
+        ),
+        child: GridView.builder(
+          shrinkWrap: true,
+          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 4,
+            crossAxisSpacing: 2,
+            mainAxisSpacing: 2,
+          ),
+          itemCount: menuList.length,
+          itemBuilder: (context, index) {
+            return MenuIconButton(menuList[index]);
+          },
+        ));
+  }
+}
--- lib/homeView/menuView/menuView.dart
+++ lib/homeView/menuView/menuView.dart
@@ -5,6 +5,7 @@ import '../../constants.dart';
 import 'menu.dart';
 
 import 'title_with_more_bbtn.dart';
+import 'favorite_menu.dart';
 
 class MenuView extends StatefulWidget {
   MenuViewState createState() => new MenuViewState();
@@ -18,22 +19,7 @@ class MenuViewState extends State<MenuView> {
             padding: EdgeInsets.all(10),
             child: Column(children: <Widget>[
               MenuTitleWithMoreBtn(title: "Favorite", press: () {}),
-              Card(
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(15.0),
-                  ),
-                  child: GridView.builder(
-                    shrinkWrap: true,
-                    gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                      crossAxisCount: 4,
-                      crossAxisSpacing: 3,
-                      mainAxisSpacing: 3,
-                    ),
-                    itemCount: menuList.length,
-                    itemBuilder: (context, index) {
-                      return MenuIconButton(menuList[index]);
-                    },
-                  )),
+              FavoriteMenu(),
             ]))); // GridView
   } // Widget
 } // Class
