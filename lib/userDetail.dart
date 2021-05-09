e0030ba72492249505478e5931b2d9e1df2958af
FlutLab.io <>
2021-05-09 13:29:29 +0000

    Autosave

--- lib/userDetail.dart
+++ lib/userDetail.dart
@@ -1,19 +1,37 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
-class UserDetail extends ChangeNotifier{
-  String _uid;
+class UserDetail extends ChangeNotifier {
+  String _uId;
   String _nickName;
   String _email;
   int _age;
   String _gender;
 
-  get uid => _uid;
-  get nickName => _nickName;
-  get email => _email;
-  get age => _age;
-  get gender => _gender;
-  
-  
+  String get uId => _uId;
+  String get nickName => _nickName;
+  String get email => _email;
+  int get age => _age;
+  String get gender => _gender;
+
+  void set uId(userUid) {
+    _uId = userUid;
+  }
+
+  void set nickName(userNickname) {
+    _nickName = userNickname;
+  }
+
+  void set email(userEmail) {
+    _email = userEmail;
+  }
+
+  void set age(userAge) {
+    _age = userAge;
+  }
+
+  void set gender(userGender) {
+    _gender = userGender;
+  }
   //notifyListeners();
 }
