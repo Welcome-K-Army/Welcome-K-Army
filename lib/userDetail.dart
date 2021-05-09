5fa8ee1b46729acb1ad1981ee10af1cc5aa73df7
FlutLab.io <>
2021-05-09 09:23:46 +0000

    Autosave

--- lib/userDetail.dart
+++ lib/userDetail.dart
@@ -4,13 +4,14 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
 class AddUser extends StatelessWidget {
+  
   final String nickName;
   final String email;
   final int age;
 
   AddUser(this.nickName, this.email, this.age);
 
-  
+    
     // Create a CollectionReference called users that references the firestore collection
     Future<void> addUser() async{
       User firebaseUser = await FirebaseAuth.instance.currentUser;
