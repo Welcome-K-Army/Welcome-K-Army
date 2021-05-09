23e7e76e625f70eaec672932dc166a5b5d6cf324
FlutLab.io <>
2021-05-09 00:26:46 +0000

    Autosave

--- lib/loginPage.dart
+++ lib/loginPage.dart
@@ -247,7 +247,7 @@ class _LoginPage extends State<LoginPage> {
             Container(
               alignment:Alignment.center,
               height: 50.0,
-              width: size.width*0.7,
+              width: size.width*0.75,
               //child: DropdownButtonHideUnderline(
                 child: DropdownButton(
                   isExpanded: true,
@@ -261,7 +261,7 @@ class _LoginPage extends State<LoginPage> {
                       DropdownMenuItem(
                         value: age,
                         child: SizedBox(
-                          width:size.width*0.7,
+                          width:size.width*0.75,
                           child:Text(age.toString()+" years old" ,textAlign: TextAlign.left,),
                         ),
                       ),
