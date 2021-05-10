c66084cc6efa3a31164a2c215af8411507e12f64
FlutLab.io <>
2021-05-10 10:51:25 +0000

    Autosave

--- lib/homeView/menuView/menu.dart
+++ lib/homeView/menuView/menu.dart
@@ -4,10 +4,10 @@ import 'package:flutter/src/widgets/basic.dart';
 import '../../calendarView/calendar.dart';
 
 final List<Menu> menuList = [
-  new Menu(name: '공지사항', icon: Icon(Icons.star, size: 30), widget: TableBasicsExample()),
-  new Menu(name: '지도검색', icon: Icon(Icons.location_pin, size: 30), widget: TableBasicsExample()),
-  new Menu(name: '입시정보', icon: Icon(Icons.search, size: 30), widget: TableBasicsExample()),
-  new Menu(name: '일정', icon: Icon(Icons.calendar_today, size: 30), widget: TableBasicsExample()),
+  new Menu(name: '공지사항', icon: Icon(Icons.star, size: 30), widget: MyHomePage(title: "welcome k army")),
+  new Menu(name: '지도검색', icon: Icon(Icons.location_pin, size: 30), widget: MyHomePage(title: "welcome k army")),
+  new Menu(name: '입시정보', icon: Icon(Icons.search, size: 30), widget: MyHomePage(title: "welcome k army")),
+  new Menu(name: '일정', icon: Icon(Icons.calendar_today, size: 30), widget: MyHomePage(title: "welcome k army")),
 ];
 
 class Menu {
