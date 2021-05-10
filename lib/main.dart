61889fa826d813cca6a508ddfc861e5330719d13
FlutLab.io <>
2021-05-10 11:09:18 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -38,7 +38,7 @@ class Splash extends StatelessWidget {
           if (snapshot.data == null) {
             return ChangeNotifierProvider<JoinOrLogin>.value(value: JoinOrLogin(), child: LoginPage()); //ChangeNotifierProvider
           } else {
-            final userDetail= Provider.of<UserDetail>(context)
+            final userDetail= Provider.of<UserDetail>(context);
             userDetail.uId=uId;
             return MainPage(uId: snapshot.data.uid, email: snapshot.data.email);
           }
