380e342b2f881613edf4cb6a41fd556fca23162e
FlutLab.io <>
2021-05-11 13:24:40 +0000

    Autosave

---
+++ lib/model/user_data_model.dart
@@ -0,0 +1,29 @@
+class UserData {
+  final String uid;
+  final String nickName;
+  final String email;
+  final int age;
+  final String creationDate;
+  final String gender;
+  
+
+  const UserData ({
+    this.uid,
+    this.nickName,
+    this.email,
+    this.creationDate,
+    this.gender;
+    this.age,
+  });
+
+  factory UserData.fromJson(Map<String, dynamic> json){
+    return UserData(
+      uid: json['UID'].toString(),
+      nickName: json['NickName'].toString(),
+      email:json['Email'].toString(),
+      age: json['Age'] as int,
+      gender:json['Gender'].toString(),
+      creationDate: json['CreationDate'].toString(),
+    );
+  }
+}
---
+++ lib/view/login_screen.dart
@@ -0,0 +1,296 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+//0xff0c9869
+
+class Login extends StatefulWidget {
+  @override
+  _LoginViewState createState() => _LoginViewState();
+}
+
+class _LoginViewState extends State<Login> {
+  final GlobalKey<FormState> _formkey = GlobalKey<FormState>();
+  TextEditingController _emailController = TextEditingController(); //email 컨트롤러
+  TextEditingController _passwordController = TextEditingController(); //password 컨트롤러
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+
+    void showAlertDialog(BuildContext context) {
+      showDialog(
+          context: context,
+          builder: (BuildContext context) {
+            TextEditingController _emailControllerField = TextEditingController();
+            return CustomAlertDialog(
+              content: Container(
+                width: size.width / 1.2,
+                height: size.height / 4.5,
+                color: Colors.white,
+                child: Column(
+                  children: <Widget>[
+                    Text("Insert Reset Email:"),
+                    TextField(
+                      controller: _emailControllerField,
+                      decoration: InputDecoration(
+                        focusedBorder: UnderlineInputBorder(
+                          borderSide: BorderSide(
+                            color: Colors.black,
+                          ),
+                        ),
+                        hintText: "something@example.com",
+                        labelText: "Email",
+                        labelStyle: TextStyle(
+                          color: Colors.black,
+                        ),
+                        hintStyle: TextStyle(
+                          color: Colors.black,
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.all(15),
+                      child: Material(
+                        elevation: 5.0,
+                        borderRadius: BorderRadius.circular(25.0),
+                        color: Color(0xff0c9869),
+                        child: MaterialButton(
+                          minWidth: size.width / 2,
+                          padding: EdgeInsets.fromLTRB(10.0, 15.0, 10.0, 15.0),
+                          child: Text(
+                            "Send Reset Email",
+                            textAlign: TextAlign.center,
+                            style: TextStyle(
+                              fontSize: 20.0,
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          onPressed: () async {
+                            try {
+                              FirebaseAuth.instance.sendPasswordResetEmail(email: _emailControllerField.text);
+                              Navigator.of(context).pop();
+                            } catch (e) {
+                              print(e);
+                              // TODO: Add snackbar reporting error
+                            }
+                          },
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            );
+          });
+    }
+
+    //상단부 이미지
+    final logo = Image.asset(
+      "lib/image/Loading.gif",
+      height: mq.size.height / 4,
+    );
+    // final logo = Expanded(
+    //   child: Padding(
+    //     padding: const EdgeInsets.only(top: 40, left: 24, right: 24),
+    //     child: FittedBox(
+    //       fit: BoxFit.contain,
+    //       child: CircleAvatar(
+    //         minRadius: 40,
+    //         backgroundImage: AssetImage("lib/image/Loading.gif"),
+    //       ), //CircleAvatar
+    //     ), //FittedBox
+    //   ), //Padding
+    // ); //Expanded
+
+    final emailField = TextFormField(
+        controller: _emailController,
+        keyboardType: TextInputType.emailAddress,
+        style: TextStyle(
+          color: Colors.white,
+        ),
+        cursorColor: Colors.white,
+        decoration: InputDecoration(
+          borderSide: BorderSide(
+            color: Colors.white,
+          ),
+          labelText: "Email",
+          hintText: "something@example.com",
+          labelStyle: TextStyle(
+            color: Colors.white,
+          ),
+          hintStyle: TextStyle(
+            color: Colors.white,
+          ),
+        ), //InputDecoration
+        validator: (String value) {
+          if (value.isEmpty) {
+            return "Please input correct Email!";
+          } else if (!RegExp(r"[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?").hasMatch(value.toString())) {
+            //이메일 정규 표현식
+            return "Not correct Email format";
+          }
+          return null;
+        }); //TextFormField 이메일
+
+    final passwordField = Column(
+      children: <Widget>[
+        TextFormField(
+            obscureText: true,
+            controller: _passwordController,
+            style: TextStyle(
+              color: Colors.white,
+            ),
+            cursorColor: Colors.white,
+            decoration: InputDecoration(
+              focusedBorder: UnderlineInputBorder(
+                borderSide: BorderSide(
+                  color: Colors.white,
+                ),
+              ),
+              hintText: "password",
+              labelText: "Password",
+              labelStyle: TextStyle(
+                color: Colors.white,
+              ),
+              hintStyle: TextStyle(
+                color: Colors.white,
+              ),
+            ),
+            validator: (String value) {
+              if (value.isEmpty) {
+                return "Please input correct Password!";
+              } else if (value.length < 8) {
+                return "Please enter a password of at least 8 digits!";
+              }
+              return null;
+            }),
+        Padding(
+          padding: EdgeInsets.all(2.0),
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: <Widget>[
+            MaterialButton(
+                child: Text(
+                  "Forgot Password",
+                  style: Theme.of(context).textTheme.caption.copyWith(color: Colors.white),
+                ),
+                onPressed: () {
+                  showAlertDialog(context);
+                }),
+          ],
+        ),
+      ],
+    );
+
+    final fields = Padding(
+      padding: EdgeInsets.only(top: 10.0),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: <Widget>[
+          emailField,
+          passwordField,
+        ],
+      ),
+    );
+
+    final loginButton = Material(
+      elevation: 5.0,
+      borderRadius: BorderRadius.circular(25.0),
+      color: Colors.white,
+      child: MaterialButton(
+        minWidth: size.width / 1.2,
+        padding: EdgeInsets.fromLTRB(10.0, 15.0, 10.0, 15.0),
+        child: Text(
+          "Login",
+          textAlign: TextAlign.center,
+          style: TextStyle(
+            fontSize: 20.0,
+            color: Colors.black,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        onPressed: () async {
+          try {
+            await Firebase.initializeApp();
+            UserCredential user = await FirebaseAuth.instance.signInWithEmailAndPassword(
+              email: _emailController.text,
+              password: _passwordController.text,
+            );
+            SharedPreferences prefs = await SharedPreferences.getInstance();
+            prefs.setString('NickName', user.user.nickName);
+            Navigator.of(context).pushNamed(AppRoutes.menu);
+          } on FirebaseAuthException catch (e) {
+            if (e.code == 'weak-password') {
+              print('The password provided is too weak.');
+            } else if (e.code == 'email-already-in-use') {
+              print('The account already exists for that email.');
+            }
+          } catch (e) {
+            print(e.toString());
+          }
+        },
+      ),
+    );
+
+    final bottom = Column(
+      mainAxisAlignment: MainAxisAlignment.start,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: <Widget>[
+        loginButton,
+        Padding(
+          padding: EdgeInsets.all(8.0),
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[
+            Text(
+              "Not a member?",
+              style: Theme.of(context).textTheme.subtitle1.copyWith(
+                    color: Colors.white,
+                  ),
+            ),
+            MaterialButton(
+              onPressed: () {
+                Navigator.of(context).pushNamed(AppRoutes.authRegister);
+              },
+              child: Text(
+                "Sign Up",
+                style: Theme.of(context).textTheme.subtitle1.copyWith(
+                      color: Colors.white,
+                      decoration: TextDecoration.underline,
+                    ),
+              ),
+            ),
+          ],
+        ),
+      ],
+    );
+
+    return Scaffold(
+      backgroundColor: Color(0xff0c9869),
+      body: Form(
+        key: _formKey,
+        child: SingleChildScrollView(
+          padding: EdgeInsets.all(36),
+          child: Container(
+            height: size.height,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: <Widget>[
+                logo,
+                fields,
+                Padding(
+                  padding: EdgeInsets.only(bottom: 150),
+                  child: bottom,
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
