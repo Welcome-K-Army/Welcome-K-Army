91c23c294a6c0ab27549b44c4ea2bc8608ccd4a0
FlutLab.io <>
2021-05-12 11:08:46 +0000

    Autosave

--- lib/views/login_screen.dart
+++ lib/views/login_screen.dart
@@ -26,12 +26,12 @@ class _LoginViewState extends State<Login> {
           builder: (BuildContext context) {
             TextEditingController _emailControllerField = TextEditingController();
             return Dialog(
-              shape : RoundedRectangleBorder ( 
-                borderRadius : 
-                    BorderRadius.circular (20.0)),
+              shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20.0)),
+              elevation: 0,
+              backgroundColor: Colors.transparent,
               child: Container(
                 width: size.width / 1.1,
-                height: size.height / 3.5,
+                height: size.height / 4.0,
                 color: Colors.white,
                 child: Column(
                   children: <Widget>[
