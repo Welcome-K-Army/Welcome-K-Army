9a6ef3857541aff40b341180048f3f4c7d7e4a56
FlutLab.io <>
2021-05-14 14:07:50 +0000

    Autosave

--- lib/analysisView/chart/radar_chart.dart
+++ lib/analysisView/chart/radar_chart.dart
@@ -15,126 +15,59 @@ class _CustomRadarChartState extends State<CustomRadarChart> {
 
   @override
   Widget build(BuildContext context) {
-    const ticks = [7, 14, 21, 28, 35];
-    var features = ["AA", "BB", "CC", "DD", "EE", "FF", "GG", "HH"];
+    const ticks = [
+      7,
+      14,
+      21,
+      28,
+      35
+    ];
+    var features = [
+      "AA",
+      "BB",
+      "CC",
+      "DD",
+      "EE",
+      "FF",
+      "GG",
+      "HH"
+    ];
     var data = [
-      [10, 20, 28, 5, 16, 15, 17, 6],
-      [15, 1, 4, 14, 23, 10, 6, 19]
+      [
+        10,
+        20,
+        28,
+        5,
+        16,
+        15,
+        17,
+        6
+      ],
+      [
+        15,
+        1,
+        4,
+        14,
+        23,
+        10,
+        6,
+        19
+      ]
     ];
 
     features = features.sublist(0, numberOfFeatures.floor());
-    data = data
-        .map((graph) => graph.sublist(0, numberOfFeatures.floor()))
-        .toList();
+    data = data.map((graph) => graph.sublist(0, numberOfFeatures.floor())).toList();
 
     return Container(
       height: 300,
       width: 300,
-      /*
-        color: darkMode ? Colors.black : Colors.white,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 8.0),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: [
-                  darkMode
-                      ? Text(
-                          'Light mode',
-                          style: TextStyle(color: Colors.white),
-                        )
-                      : Text(
-                          'Dark mode',
-                          style: TextStyle(color: Colors.black),
-                        ),
-                  Switch(
-                    value: this.darkMode,
-                    onChanged: (value) {
-                      setState(() {
-                        darkMode = value;
-                      });
-                    },
-                  ),
-                ],
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 8.0),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: [
-                  useSides
-                      ? Text(
-                          'Polygon border',
-                          style: darkMode
-                              ? TextStyle(color: Colors.white)
-                              : TextStyle(color: Colors.black),
-                        )
-                      : Text(
-                          'Circular border',
-                          style: darkMode
-                              ? TextStyle(color: Colors.white)
-                              : TextStyle(color: Colors.black),
-                        ),
-                  Switch(
-                    value: this.useSides,
-                    onChanged: (value) {
-                      setState(() {
-                        useSides = value;
-                      });
-                    },
-                  ),
-                ],
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 8.0),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.end,
-                children: <Widget>[
-                  Text(
-                    'Number of features',
-                    style: TextStyle(
-                        color: darkMode ? Colors.white : Colors.black),
-                  ),
-                  Expanded(
-                    child: Slider(
-                      value: this.numberOfFeatures,
-                      min: 3,
-                      max: 8,
-                      divisions: 5,
-                      onChanged: (value) {
-                        setState(() {
-                          numberOfFeatures = value;
-                        });
-                      },
-                    ),
-                  ),
-                ],
-              ),
-            ),
-            Expanded(
-              child: darkMode
-                  ? RadarChart.dark(
-                      ticks: ticks,
-                      features: features,
-                      data: data,
-                      reverseAxis: true,
-                      useSides: useSides,
-                    )
-                  : RadarChart.light(
-                      ticks: ticks,
-                      features: features,
-                      data: data,
-                      reverseAxis: true,
-                      useSides: useSides,
-                    ),
-            ),
-          ],
-        ),
-        */
-      );
+      child: RadarChart(
+        ticks: ticks,
+        features: features,
+        data: data,
+        reverseAxis: true,
+        useSides: useSides,
+      ),
+    );
   }
 }
