65777d8272a7c069d09a9d212c0b63ff6077e11b
FlutLab.io <>
2021-05-14 14:18:11 +0000

    Autosave

--- lib/analysisView/analysisView.dart
+++ lib/analysisView/analysisView.dart
@@ -36,6 +36,7 @@ class AnalysisViewState extends State<AnalysisView> {
 
   @override
   Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
     return Scaffold(
         appBar: AppBar(
           title: Text("Analysis"),
@@ -55,7 +56,7 @@ class AnalysisViewState extends State<AnalysisView> {
                         Container(
                           child: CustomPaint(
                             // CustomPaint를 그리고 이 안에 차트를 그려줍니다..
-                            size: Size(75, 75), // CustomPaint의 크기는 가로 세로 150, 150으로 합니다.
+                            size: Size(size.width/4, size.width/4), // CustomPaint의 크기는 가로 세로 150, 150으로 합니다.
                             painter: PieChart(
                                 percentage: 50, // 파이 차트가 얼마나 칠해져 있는지 정하는 변수입니다.
                                 textScaleFactor: 1.0, // 파이 차트에 들어갈 텍스트 크기를 정합니다.
@@ -75,7 +76,7 @@ class AnalysisViewState extends State<AnalysisView> {
                         Container(
                           child: CustomPaint(
                             // CustomPaint를 그리고 이 안에 차트를 그려줍니다..
-                            size: Size(150, 150), // CustomPaint의 크기는 가로 세로 150, 150으로 합니다.
+                            size: Size(size.width/4, size.width/4), // CustomPaint의 크기는 가로 세로 150, 150으로 합니다.
                             painter: PieChart(
                                 percentage: 50, // 파이 차트가 얼마나 칠해져 있는지 정하는 변수입니다.
                                 textScaleFactor: 1.0, // 파이 차트에 들어갈 텍스트 크기를 정합니다.
