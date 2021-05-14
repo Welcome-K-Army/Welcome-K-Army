144d66d62d0b02bde86dec302352d3313bd3cec3
FlutLab.io <>
2021-05-14 13:25:44 +0000

    Autosave

--- lib/analysisView/customSwitch.dart
+++ lib/analysisView/customSwitch.dart
@@ -13,43 +13,36 @@ class _CustomSwitchState extends State<CustomSwitch> {
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-      children: <Widget>[
-        FlutterSwitch(
-          activeText: "Pie",
-          inactiveText: "Radar",
-          width: 100.0,
-          height: 55.0,
-          toggleSize: 45.0,
-          value: status2,
-          borderRadius: 30.0,
-          padding: 2.0,
-          toggleColor: Color.fromRGBO(225, 225, 225, 1),
-          switchBorder: Border.all(
-            color: Color.fromRGBO(2, 107, 206, 1),
-            width: 6.0,
-          ),
+    return Center(
+      child: FlutterSwitch(
+        activeText: "Pie",
+        inactiveText: "Radar",
+        width: 150.0,
+        height: 55.0,
+        toggleSize: 75.0,
+        value: status2,
+        borderRadius: 30.0,
+        padding: 1.0,
+        toggleColor: Color.fromRGBO(225, 225, 225, 1),
+        switchBorder: Border.all(
+          color: Color(0xFF0C9869),
+          width: 6.0,
+        ),
+        /*
           toggleBorder: Border.all(
             color: Color.fromRGBO(2, 107, 206, 1),
             width: 5.0,
           ),
-          activeColor: Color.fromRGBO(51, 226, 255, 1),
-          inactiveColor: Colors.black38,
-          showOnOff: true,
-          onToggle: (val) {
-            setState(() {
-              status2 = val;
-            });
-          },
-        ),
-        Container(
-          alignment: Alignment.centerRight,
-          child: Text(
-            "Value: $status2",
-          ),
-        ),
-      ],
+          */
+        activeColor: Color(0xFF0C9869),
+        inactiveColor: Color(0xFFEF9A9A),
+        showOnOff: true,
+        onToggle: (val) {
+          setState(() {
+            status2 = val;
+          });
+        },
+      ),
     );
   }
 }
