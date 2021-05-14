60b5d3c8b2ff8aa05a872da5a8ad1723ce7d4d8a
FlutLab.io <>
2021-05-14 11:15:18 +0000

    Autosave

--- lib/analysisView/analysisView.dart
+++ lib/analysisView/analysisView.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/basic.dart';
+import 'package:flutter_sketcher_app/ui/chart_page.dart';
+
+import 'pie_chart.dart';
 
 class AnalysisView extends StatefulWidget {
   AnalysisViewState createState() => new AnalysisViewState();
@@ -18,9 +21,22 @@ class AnalysisViewState extends State<AnalysisView> {
           children: [
             Text("전체 분석"),
             Container(
-              height: 150,
-              color: Colors.amberAccent,
-            ),
+        padding: const EdgeInsets.symmetric(vertical: 8.0),
+        child: Center(
+          child: Column(
+            children: <Widget>[
+              Container(
+                child: CustomPaint( // CustomPaint를 그리고 이 안에 차트를 그려줍니다.. 
+                  size: Size(150, 150), // CustomPaint의 크기는 가로 세로 150, 150으로 합니다. 
+                  painter: PieChart(percentage: 50, // 파이 차트가 얼마나 칠해져 있는지 정하는 변수입니다.
+                      textScaleFactor: 1.0, // 파이 차트에 들어갈 텍스트 크기를 정합니다. 
+                      textColor: Colors.blueGrey),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
             Text("항목별 분석"),
             Row(
               children: [
