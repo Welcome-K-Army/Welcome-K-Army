c8f307579bb6d65b5ef821a327073a3708c3ae65
FlutLab.io <>
2021-05-14 11:37:07 +0000

    Autosave

--- lib/settings/profile_menu.dart
+++ lib/settings/profile_menu.dart
@@ -34,3 +34,48 @@ class Profile_menu extends StatelessWidget {
     ); //Column;
   }
 }
+
+class ProfileMenu extends StatelessWidget {
+  const ProfileMenu({
+    Key key,
+    @required this.text, //@required은 기본값이 없고 null이 아닐경우에 사용/
+    @required this.icon, 
+    @required this.press,
+  }) : super(key: key); //기본생성자 호출
+
+  final String text;
+  final Icon icon;
+  final VoidCallback press;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+          padding:const EdgeInsets.symmetric(horizontal: 20,vertical: 10),
+          child:Card(
+              shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15.0)),
+              elevation: 5,
+              child:FlatButton(
+              padding: EdgeInsets.all(20),
+              
+              color: Color(0xFFF5F6F9),
+              onPressed : press,
+              child:Row(
+                children:[
+                  icon,
+                  SizedBox(width: 20),
+                  Expanded(
+                    child:Text(
+                      text,
+                      style: Theme.of(context).textTheme.bodyText1,
+                      
+                    ),//Text
+                  ),//Expanded
+                  Icon(Icons.east)
+                ],
+              ),//Row
+            ),//FlatButton
+          ),//Card
+
+        );//Padding;
+  }
+}
