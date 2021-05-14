bea78f586eb36447dc9133765b007078a3807645
FlutLab.io <>
2021-05-14 11:31:26 +0000

    Autosave

--- lib/analysisView/analysisView.dart
+++ lib/analysisView/analysisView.dart
@@ -54,17 +54,41 @@ class AnalysisViewState extends State<AnalysisView> {
             Text("항목별 분석"),
             Row(
               children: [
-                Container(
-                  height: 150,
-                  width: 150,
-                  color: Colors.blueAccent,
+Container(
+        padding: const EdgeInsets.symmetric(vertical: 8.0),
+        child: Center(
+          child: Column(
+            children: <Widget>[
+              Container(
+                child: CustomPaint( // CustomPaint를 그리고 이 안에 차트를 그려줍니다.. 
+                  size: Size(150, 150), // CustomPaint의 크기는 가로 세로 150, 150으로 합니다. 
+                  painter: PieChart(percentage: 50, // 파이 차트가 얼마나 칠해져 있는지 정하는 변수입니다.
+                      textScaleFactor: 1.0, // 파이 차트에 들어갈 텍스트 크기를 정합니다. 
+                      textColor: Colors.blueGrey),
                 ),
+              ),
+            ],
+          ),
+        ),
+      ),
                 Text("hi"),
-                Container(
-                  height: 150,
-                  width: 150,
-                  color: Colors.blueAccent,
+Container(
+        padding: const EdgeInsets.symmetric(vertical: 8.0),
+        child: Center(
+          child: Column(
+            children: <Widget>[
+              Container(
+                child: CustomPaint( // CustomPaint를 그리고 이 안에 차트를 그려줍니다.. 
+                  size: Size(150, 150), // CustomPaint의 크기는 가로 세로 150, 150으로 합니다. 
+                  painter: PieChart(percentage: 50, // 파이 차트가 얼마나 칠해져 있는지 정하는 변수입니다.
+                      textScaleFactor: 1.0, // 파이 차트에 들어갈 텍스트 크기를 정합니다. 
+                      textColor: Colors.blueGrey),
                 ),
+              ),
+            ],
+          ),
+        ),
+      ),
                 Text("hi"),
               ]
             ),
