c423aa8bdf5c6d1b3b769ef960c1cc9376992217
FlutLab.io <>
2021-05-14 11:34:57 +0000

    Autosave

--- lib/settings/profile_menu.dart
+++ lib/settings/profile_menu.dart
@@ -1,92 +1,9 @@
 import 'package:flutter/material.dart';
-import '../views/menu.dart';
 
-class Profile_menu extends StatefulWidget {
-  @override
-  _Profile_menuState createState() => _Profile_menuState();
-}
 
-class _Profile_menuState extends State<Profile_menu> {
+class Profile_menu extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-        child: Column(
-      children: [
-        const SizedBox(height: 50),
-        SizedBox(height: 25),
-        ProfileMenu(
-          //계정 수정 네비게이터
-          icon: Icon(Icons.account_circle),
-          text: "My Account",
-          press: () {
-            // Navigator.push(context,MaterialPageRoute(builder:(context) =>);//currentOnlineUserId넘겨줘야됨
-          },
-        ),
-        ProfileMenu(
-            //관심 직책 네비게이터
-            icon: Icon(Icons.library_books),
-            text: "관심직책",
-            press: () {}),
-        ProfileMenu(
-            //도움말 네비게이터
-            icon: Icon(Icons.help_outline),
-            text: "help",
-            press: () {}),
-        ProfileMenu(
-            //로그아웃 네비게이터
-            icon: Icon(Icons.logout),
-            text: "Logout",
-            press: () {}),
-      ],
-    ) //Column;
-        );
-  }
-}
-
-class ProfileMenu extends StatefulWidget {
-  @override
-  _ProflieMenuState createState() => _ProfileMenuState();
-}
-
-class _ProfileMenuState extends State<ProfileMenu> {
-  const _ProfileMenuState({
-    Key key,
-    @required this.text, //@required은 기본값이 없고 null이 아닐경우에 사용/
-    @required this.icon,
-    @required this.press,
-  }) : super(key: key); //기본생성자 호출
-
-  final String text;
-  final Icon icon;
-  final VoidCallback press;
-
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-      child: Card(
-        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15.0)),
-        elevation: 5,
-        child: FlatButton(
-          padding: EdgeInsets.all(20),
-
-          color: Color(0xFFF5F6F9),
-          onPressed: press,
-          child: Row(
-            children: [
-              icon,
-              SizedBox(width: 20),
-              Expanded(
-                child: Text(
-                  text,
-                  style: Theme.of(context).textTheme.bodyText1,
-                ), //Text
-              ), //Expanded
-              Icon(Icons.east)
-            ],
-          ), //Row
-        ), //FlatButton
-      ), //Card
-    ); //Padding;
+    return null;
   }
 }
