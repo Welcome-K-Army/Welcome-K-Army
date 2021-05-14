615249a78a63186d29dd988ea832708ed13af9c4
FlutLab.io <>
2021-05-14 10:48:53 +0000

    Autosave

--- lib/analysisView/analysisView.dart
+++ lib/analysisView/analysisView.dart
@@ -8,6 +8,32 @@ class AnalysisView extends StatefulWidget {
 class AnalysisStateView extends State<AnalysisView> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Analysis"),
+      ),
+      body: SingleChildScrollView(
+        scrollDirection: Axis.vertical,
+        child: Column(
+          children: [
+            Text("전체 분석"),
+            Container(
+              height: 150,
+              color: Colors.amberAccent,
+            ),
+            Text("항목별 분석"),
+            Container(
+              height: 150,
+              color: Colors.blueAccent,
+            ),
+            Text("경쟁률"),
+            Container(
+              height: 150,
+              color: Colors.redAccent,
+            ),
+          ]
+        )
+      )
+    );
   }
 }
