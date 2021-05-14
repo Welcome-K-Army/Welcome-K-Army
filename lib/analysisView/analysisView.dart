2589517da9ddaae6de7681fca984dd2dea6981ae
FlutLab.io <>
2021-05-14 11:27:54 +0000

    Autosave

--- lib/analysisView/analysisView.dart
+++ lib/analysisView/analysisView.dart
@@ -1,13 +1,28 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/basic.dart';
 
-import 'pie_chart.dart';
+import './chart/pie_chart.dart';
+import './chart/line_chart.dart';
+import './chart/bar_chart.dart';
 
 class AnalysisView extends StatefulWidget {
   AnalysisViewState createState() => new AnalysisViewState();
 }
 
 class AnalysisViewState extends State<AnalysisView> {
+  List<double> points = [50, 90, 1003, 500, 150, 120, 200, 80];
+
+  List<String> labels = [
+    "2012",
+    "2013",
+    "2014",
+    "2015",
+    "2016",
+    "2017",
+    "2018",
+    "2019",
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -55,9 +70,20 @@ class AnalysisViewState extends State<AnalysisView> {
             ),
             Text("경쟁률"),
             Container(
-              height: 150,
-              color: Colors.redAccent,
-            ),
+                child: CustomPaint(
+                  size: Size(150, 150),
+                  painter: LineChart(points: points, pointSize: 5.0, pointColor: Colors.pinkAccent, lineColor: Colors.pinkAccent, lineWidth: 2.0),
+                ),
+              ),
+              Padding(padding: const EdgeInsets.symmetric(vertical: 8.0)),
+              Container(
+                child: CustomPaint(
+                    size: Size(250, 250),
+                    foregroundPainter: BarChart(
+                        data: points,
+                        labels: labels,
+                        color: Colors.pinkAccent)),
+              ),
           ]
         )
       )
