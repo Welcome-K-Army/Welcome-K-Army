aee628024c8a55a610e5254c805f3839a75528bd
FlutLab.io <>
2021-05-14 14:55:08 +0000

    Add switch with pie and radar

--- lib/analysisView/pie_chart_with_text.dart
+++ lib/analysisView/pie_chart_with_text.dart
@@ -27,49 +27,47 @@ class _PieChartWithTextState extends State<PieChartWithText> {
         title,
         style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
       ),
-      Row(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly, 
-        children: [
-          Container(
-            padding: const EdgeInsets.symmetric(vertical: 8.0),
-            child: Center(
-              child: Column(
-                children: <Widget>[
-                  Container(
-                    child: CustomPaint(
-                      // CustomPaint를 그리고 이 안에 차트를 그려줍니다..
-                      size: Size(size.width / 4, size.width / 4), // CustomPaint의 크기는 가로 세로 150, 150으로 합니다.
-                      painter: PieChart(
-                          percentage: 50, // 파이 차트가 얼마나 칠해져 있는지 정하는 변수입니다.
-                          textScaleFactor: 1.0, // 파이 차트에 들어갈 텍스트 크기를 정합니다.
-                          textColor: Colors.blueGrey),
-                    ),
+      Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: [
+        Container(
+          padding: const EdgeInsets.symmetric(vertical: 8.0),
+          child: Center(
+            child: Column(
+              children: <Widget>[
+                Container(
+                  child: CustomPaint(
+                    // CustomPaint를 그리고 이 안에 차트를 그려줍니다..
+                    size: Size(size.width / 4, size.width / 4), // CustomPaint의 크기는 가로 세로 150, 150으로 합니다.
+                    painter: PieChart(
+                        percentage: 50, // 파이 차트가 얼마나 칠해져 있는지 정하는 변수입니다.
+                        textScaleFactor: 1.0, // 파이 차트에 들어갈 텍스트 크기를 정합니다.
+                        textColor: Colors.blueGrey),
                   ),
-                ],
-              ),
+                ),
+              ],
             ),
           ),
-          Text("hi"),
-          Container(
-            padding: const EdgeInsets.symmetric(vertical: 8.0),
-            child: Center(
-              child: Column(
-                children: <Widget>[
-                  Container(
-                    child: CustomPaint(
-                      // CustomPaint를 그리고 이 안에 차트를 그려줍니다..
-                      size: Size(size.width / 4, size.width / 4), // CustomPaint의 크기는 가로 세로 150, 150으로 합니다.
-                      painter: PieChart(
-                          percentage: 50, // 파이 차트가 얼마나 칠해져 있는지 정하는 변수입니다.
-                          textScaleFactor: 1.0, // 파이 차트에 들어갈 텍스트 크기를 정합니다.
-                          textColor: Colors.blueGrey),
-                    ),
+        ),
+        Text("hi"),
+        Container(
+          padding: const EdgeInsets.symmetric(vertical: 8.0),
+          child: Center(
+            child: Column(
+              children: <Widget>[
+                Container(
+                  child: CustomPaint(
+                    // CustomPaint를 그리고 이 안에 차트를 그려줍니다..
+                    size: Size(size.width / 4, size.width / 4), // CustomPaint의 크기는 가로 세로 150, 150으로 합니다.
+                    painter: PieChart(
+                        percentage: 50, // 파이 차트가 얼마나 칠해져 있는지 정하는 변수입니다.
+                        textScaleFactor: 1.0, // 파이 차트에 들어갈 텍스트 크기를 정합니다.
+                        textColor: Colors.blueGrey),
                   ),
-                ],
-              ),
+                ),
+              ],
             ),
           ),
-          Text("hi"),
+        ),
+        Text("hi"),
       ]),
     ]);
   }
