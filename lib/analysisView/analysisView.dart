d49adfd542b3bc7b1eab0f95f8875ce9c0481a26
FlutLab.io <>
2021-05-14 10:53:19 +0000

    Autosave

--- lib/analysisView/analysisView.dart
+++ lib/analysisView/analysisView.dart
@@ -1,5 +1,5 @@
-import "pacakge:flutter/material.dart";
-import "package:flutter/src/widgets/basic.dart";
+import 'pacakge:flutter/material.dart';
+import 'package:flutter/src/widgets/basic.dart';
 
 class AnalysisView extends StatefulWidget {
   AnalysisView createState() => new AnalysisStateView();
@@ -22,9 +22,21 @@ class AnalysisStateView extends State<AnalysisView> {
               color: Colors.amberAccent,
             ),
             Text("항목별 분석"),
-            Container(
-              height: 150,
-              color: Colors.blueAccent,
+            Row(
+              children: [
+                Container(
+                  height: 150,
+                  width: 150,
+                  color: Colors.blueAccent,
+                ),
+                Text("hi"),
+                Container(
+                  height: 150,
+                  width: 150,
+                  color: Colors.blueAccent,
+                ),
+                Text("hi"),
+              ]
             ),
             Text("경쟁률"),
             Container(
