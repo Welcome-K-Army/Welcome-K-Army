81ee8f6a583137796e67c174f6652c0476e76feb
FlutLab.io <>
2021-05-15 14:44:14 +0000

    Autosave

--- lib/analysisView/switch_with_pie_and_radar_chart.dart
+++ lib/analysisView/switch_with_pie_and_radar_chart.dart
@@ -34,15 +34,15 @@ class _SwitchWithPieAndRadarChartState extends State<SwitchWithPieAndRadarChart>
             child: Column(
               children: <Widget>[
                 Container(
-                  height: 400,
-                  width: 400,
+                  height: 200,
+                  width: 200,
                   child: 
                 status
                     ? DonutAutoLabelChart.withSampleData()
                     : Container(
                         child: CustomPaint(
                           // CustomPaint를 그리고 이 안에 차트를 그려줍니다..
-                          size: Size(300, 300), // CustomPaint의 크기는 가로 세로 150, 150으로 합니다.
+                          size: Size(200, 200), // CustomPaint의 크기는 가로 세로 150, 150으로 합니다.
                           painter: PieChart(
                               percentage: 50, // 파이 차트가 얼마나 칠해져 있는지 정하는 변수입니다.
                               textScaleFactor: 1.0, // 파이 차트에 들어갈 텍스트 크기를 정합니다.
