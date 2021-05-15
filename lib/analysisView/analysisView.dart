1ce36f574f7e1118dbdd41370e9bd4e3ef6f9ce7
FlutLab.io <>
2021-05-15 07:33:26 +0000

    Mix line and bar chart

--- lib/analysisView/analysisView.dart
+++ lib/analysisView/analysisView.dart
@@ -1,15 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/basic.dart';
 
-import './chart/pie_chart.dart';
-import './chart/line_chart.dart';
-import './chart/bar_chart.dart';
-import './chart/radar_chart.dart';
-
-import 'switch_with_pie_and_radar_chart.dart';
-import 'pie_chart_with_text.dart';
-import 'mix_line_and_bar_chart.dart';
-
 class AnalysisView extends StatefulWidget {
   AnalysisViewState createState() => new AnalysisViewState();
 }
