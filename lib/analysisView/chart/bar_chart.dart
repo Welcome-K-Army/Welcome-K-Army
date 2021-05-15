1b1399f4ccf4d49cead71d095d42ec050fdc3803
FlutLab.io <>
2021-05-15 13:18:56 +0000

    Autosave

--- lib/analysisView/chart/bar_chart.dart
+++ lib/analysisView/chart/bar_chart.dart
@@ -104,7 +104,7 @@ class BarChart extends CustomPainter {
     double valueSpace = maxValue.toInt() / numberOfLabels;
     double fontSize = calculateFontSize(maxValue, size, xAxis: false);
 
-    for(int index = 0; index < numberOfLabels; index++) drawYText(canvas, valueSpace * index, fontSize, bottomY+heightSpace * index);
+    for(int index = 0; index < numberOfLabels; index++) drawYText(canvas, "${valueSpace * index}", fontSize, bottomY+heightSpace * index);
   }
 
   double calculateFontSize(String value, Size size, {bool xAxis}) {
