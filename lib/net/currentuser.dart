605409dea7fa0739f84e0512534e5527362000cd
FlutLab.io <>
2021-05-16 06:02:50 +0000

    Autosave

--- lib/net/currentuser.dart
+++ lib/net/currentuser.dart
@@ -2,9 +2,8 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
 
-final FirebaseAuth auth=FirebaseAuth.instance;
-
-void inputData() {
-  final User user = auth.currentUser;
-  final uid = user.uid;
+class DatabaseService{
+  
+  final CollectionReference UserDetail = Firestore.instance.collection('UserDetail');
+  
 }
