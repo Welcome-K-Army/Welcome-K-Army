63e8d7fe50adaa3534af26ca94e45500468708b8
FlutLab.io <>
2021-05-17 14:15:52 +0000

    Autosave

--- lib/analysisView/custom_toggle_button.dart
+++ lib/analysisView/custom_toggle_button.dart
@@ -25,8 +25,7 @@ class _CustomToggleButtonState extends State<CustomToggleButton> {
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: 
-        Container(
+        child: Container(
             padding: EdgeInsets.all(4),
             width: 170,
             decoration: BoxDecoration(border: Border.all(color: borderColor), borderRadius: BorderRadius.circular(22.0)),
@@ -49,7 +48,13 @@ class _CustomToggleButtonState extends State<CustomToggleButton> {
               ),
               SizedBox(width: 15),
               ElevatedButton(
-                style: ButtonStyle(foregroundColor: MaterialStateProperty.all<Color>(Colors.white), backgroundColor: MaterialStateProperty.all<Color>(secondButtonColor), shape: MaterialStateProperty.all<RoundedRectangleBorder>(RoundedRectangleBorder(borderRadius: BorderRadius.circular(22.0), side: BorderSide(color: secondButtonColor)))),
+                style: ButtonStyle(
+                  foregroundColor: MaterialStateProperty.all<Color>(Colors.white), 
+                  backgroundColor: MaterialStateProperty.all<Color>(secondButtonColor), 
+                  shape: MaterialStateProperty.all<RoundedRectangleBorder>(RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(22.0), 
+                    side: BorderSide(color: secondButtonColor)
+                  ))),
                 onPressed: () {
                   setState(() {
                     status = true;
@@ -58,8 +63,6 @@ class _CustomToggleButtonState extends State<CustomToggleButton> {
                   });
                 },
               ),
-            ]))
-      ]),
-    );
+            ])));
   }
 }
