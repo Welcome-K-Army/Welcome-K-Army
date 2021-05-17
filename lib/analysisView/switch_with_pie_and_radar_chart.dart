24e1a7028ed951f50e2c8a55dba8d8e7d6703300
FlutLab.io <>
2021-05-17 13:13:28 +0000

    Autosave

--- lib/analysisView/switch_with_pie_and_radar_chart.dart
+++ lib/analysisView/switch_with_pie_and_radar_chart.dart
@@ -12,10 +12,10 @@ class SwitchWithPieAndRadarChart extends StatefulWidget {
 }
 
 class _SwitchWithPieAndRadarChartState extends State<SwitchWithPieAndRadarChart> {
-  String title; 
- 
+  String title;
+
   _SwitchWithPieAndRadarChartState({this.title});
-  
+
   bool status = false;
   Color _textColor = Colors.black;
 
@@ -24,26 +24,25 @@ class _SwitchWithPieAndRadarChartState extends State<SwitchWithPieAndRadarChart>
     return Center(
       child: Column(children: [
         Text(
-              title,
-              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+          title,
+          style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
         ),
         Container(
           padding: const EdgeInsets.symmetric(vertical: 8.0),
           child: Center(
             child: Column(
               children: <Widget>[
-                Container(
-                  height: 200,
-                  width: 200,
-                  child: 
-                status
-                    ? CustomRadarChart()
-                    : DonutAutoLabelChart.withSampleData()
-                )
+                Container(height: 200, width: 200, child: status ? CustomRadarChart() : DonutAutoLabelChart.withSampleData())
               ],
             ),
           ),
         ),
+        Row(mainAxisAlignment: MainAxisAlignment.end, children: [
+          TextButton(child: Text("Add to cart".toUpperCase(), style: TextStyle(fontSize: 14)), style: ButtonStyle(padding: MaterialStateProperty.all<EdgeInsets>(EdgeInsets.all(15)), foregroundColor: MaterialStateProperty.all<Color>(Colors.red), shape: MaterialStateProperty.all<RoundedRectangleBorder>(RoundedRectangleBorder(borderRadius: BorderRadius.circular(18.0), side: BorderSide(color: Colors.red)))), onPressed: () => null),
+          SizedBox(width: 10),
+          ElevatedButton(child: Text("Buy now".toUpperCase(), style: TextStyle(fontSize: 14)), style: ButtonStyle(foregroundColor: MaterialStateProperty.all<Color>(Colors.white), backgroundColor: MaterialStateProperty.all<Color>(Colors.red), shape: MaterialStateProperty.all<RoundedRectangleBorder>(RoundedRectangleBorder(borderRadius: BorderRadius.zero, side: BorderSide(color: Colors.red)))), onPressed: () => null)
+        ])
+        /*
         FlutterSwitch(
           activeText: "Pie",
           inactiveText: "Radar",
@@ -54,7 +53,6 @@ class _SwitchWithPieAndRadarChartState extends State<SwitchWithPieAndRadarChart>
           borderRadius: 30.0,
           padding: 1.0,
           toggleColor: Color.fromRGBO(225, 225, 225, 1),
-          /*
         switchBorder: Border.all(
           color: Color(0xFF0C9869),
           width: 6.0,
@@ -63,7 +61,6 @@ class _SwitchWithPieAndRadarChartState extends State<SwitchWithPieAndRadarChart>
             color: Color.fromRGBO(2, 107, 206, 1),
             width: 5.0,
           ),
-          */
           activeColor: Color(0xFFEF9A9A),
           inactiveColor: Color(0xFF0C9869),
           showOnOff: true,
@@ -73,6 +70,7 @@ class _SwitchWithPieAndRadarChartState extends State<SwitchWithPieAndRadarChart>
             });
           },
         ),
+        */
       ]),
     );
   }
