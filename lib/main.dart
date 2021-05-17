b5dcfc3eee10ce4b1a5abbdac2e615430f3f139f
FlutLab.io <>
2021-05-17 10:47:04 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'theme/routes.dart';
 import 'views/opening_screen.dart';
 import 'model/user_data_model.dart';
