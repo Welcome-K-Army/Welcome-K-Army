8f69f2e6c0bcf5c653b2ab4c5567b0928b03b73f
FlutLab.io <>
2021-05-18 03:35:37 +0000

    Autosave

--- lib/net/firebase.dart
+++ lib/net/firebase.dart
@@ -21,7 +21,7 @@ Future<void> userSetup(String nickName, String email, String gender, int age) as
   return;
 }
 
-Future<void> userLoad(UserData userData) async {
+Future<UserData> userLoad() async {
   CollectionReference users = FirebaseFirestore.instance.collection('UserDetail');
   FirebaseAuth auth = FirebaseAuth.instance;
   String uid = auth.currentUser.uid.toString();
@@ -29,14 +29,13 @@ Future<void> userLoad(UserData userData) async {
     if (documentSnapshot.exists) {
       print(documentSnapshot.data().toString());
       Map<String, dynamic> data = documentSnapshot.data();
-      final userData = UserData.fromJson(data);
-      print(userData.gender);
-      userData.update();
+      return UserData.fromJson(data);
+      
     } else {
       print('no data');
+      return null;
     }
   });
-  return;
 }
 
 Future<void> userUpdate(String nickName, String email, String gender, int age) async {
