01b78840b2e746cf8a92c21f6fa3f13c151bbede
FlutLab.io <>
2021-05-18 13:24:11 +0000

    Autosave

--- lib/analysisView/chart/donut_auto_label_chart.dart
+++ lib/analysisView/chart/donut_auto_label_chart.dart
@@ -1,8 +1,9 @@
 import 'dart:math';
 // EXCLUDE_FROM_GALLERY_DOCS_END
 import 'package:charts_flutter/flutter.dart' as charts;
-import 'package:charts_common/flutter.dart' as common;
+import 'package:charts_common/common.dart' as common;
 import 'package:flutter/material.dart';
+
 class DonutAutoLabelChart extends StatelessWidget {
   final List<charts.Series> seriesList;
   final bool animate;
