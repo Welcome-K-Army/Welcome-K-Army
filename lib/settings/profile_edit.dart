d80c741ff2b7440636296dc330c66cbdd408789c
FlutLab.io <>
2021-05-18 13:35:01 +0000

    Autosave

--- lib/settings/profile_edit.dart
+++ lib/settings/profile_edit.dart
@@ -194,7 +194,9 @@ class _EditProfileState extends State<EditProfile> {
       child: Stack(
         children: [
           Container(
-            child : Image.file(_image),
+            if(_image != null){
+              child : Image.file(_image),
+            },
             width: 130,
             height: 130,
             decoration: BoxDecoration(
