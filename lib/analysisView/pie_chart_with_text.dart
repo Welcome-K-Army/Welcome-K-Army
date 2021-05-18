4cd603e4be0f12a7868fd4e6065256ceb6e41c15
FlutLab.io <>
2021-05-18 13:42:57 +0000

    Autosave

--- lib/analysisView/pie_chart_with_text.dart
+++ lib/analysisView/pie_chart_with_text.dart
@@ -54,6 +54,32 @@ class _PieChartWithTextState extends State<PieChartWithText> {
           width: size.width / 6,
           child: Text("hi"),
         ),
+        Container(
+          height: size.width / 3,
+          width : size.width / 3,
+          padding: const EdgeInsets.symmetric(vertical: 8.0),
+          child: Center(
+            child: DonutAutoLabelChart.withSampleData()
+          ),
+        ),
+        Container(
+          height: size.width / 3,
+          width: size.width / 6,
+          child: Text("hi"),
+        ),
+        Container(
+          height: size.width / 3,
+          width : size.width / 3,
+          padding: const EdgeInsets.symmetric(vertical: 8.0),
+          child: Center(
+            child: DonutAutoLabelChart.withSampleData()
+          ),
+        ),
+        Container(
+          height: size.width / 3,
+          width: size.width / 6,
+          child: Text("hi"),
+        ),                
       ]),
     ]);
   }
