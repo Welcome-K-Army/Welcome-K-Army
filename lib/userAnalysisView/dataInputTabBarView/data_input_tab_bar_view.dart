27013b8a6ba80382a07f11c19dc8914f33e039a9
FlutLab.io <>
2021-05-18 14:17:25 +0000

    Autosave

--- lib/userAnalysisView/dataInputTabBarView/data_input_tab_bar_view.dart
+++ lib/userAnalysisView/dataInputTabBarView/data_input_tab_bar_view.dart
@@ -9,17 +9,20 @@ class DataInputTabBarViewState extends State<DataInputTabBarView> {
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
-    return Scaffold(
+    return DefaultTabController(
+      length: 3,
+      child: Scaffold(
         appBar: AppBar(
-          title: Text("userDataInput"),
+          title: Text("UserDataInput"),
         ),
         body: TabBarView(
           children: <Widget> [
             Tab(text: "1차시험"),
             Tab(text: "2차시험"),
-            Tab(text: "내신 및 수능"),
+            Tab(text: "내신 및 수능"),            
           ]
         )
+      )
     );
   }
 }
