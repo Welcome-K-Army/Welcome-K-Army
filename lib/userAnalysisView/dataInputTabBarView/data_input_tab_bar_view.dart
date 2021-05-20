1bc7ec5ad1518a7736bc79a8c36320e097ce35d9
FlutLab.io <>
2021-05-20 10:49:16 +0000

    Autosave

--- lib/userAnalysisView/dataInputTabBarView/data_input_tab_bar_view.dart
+++ lib/userAnalysisView/dataInputTabBarView/data_input_tab_bar_view.dart
@@ -58,7 +58,29 @@ class DataInputTabBarViewState extends State<DataInputTabBarView> with SingleTic
                 prefixIcon: Icon(Icons.perm_identity), 
               ), 
             ),
-            Text("1차 시험"),
+            DropdownButton<String>(
+              value: dropdownValue,
+              icon: const Icon(Icons.arrow_downward),
+              iconSize: 24,
+              elevation: 16,
+              style: const TextStyle(color: Colors.deepPurple),
+              underline: Container(
+                height: 2,
+                color: Colors.deepPurpleAccent,
+              ),
+              onChanged: (String? newValue) {
+                setState(() {
+                  dropdownValue = newValue!;
+                });
+              },
+              items: <String>['One', 'Two', 'Free', 'Four']
+                  .map<DropdownMenuItem<String>>((String value) {
+                return DropdownMenuItem<String>(
+                  value: value,
+                  child: Text(value),
+                );
+              }).toList(),
+            ),
             Text("2차 시험"),
             Text("내신 및 수능"),
           ]
