c45ed4c768ff7512f13a8aa11813b35c29d801ff
FlutLab.io <>
2021-05-20 14:31:23 +0000

    Autosave

--- lib/settings/profile_edit.dart
+++ lib/settings/profile_edit.dart
@@ -256,7 +256,7 @@ class _EditProfileState extends State<EditProfile> {
                       //DB에서 사진가져와야댐
                       fit: BoxFit.cover, //원본크기 유지
                       
-                      image:_image != null ?FileImage(_image):NetworkImage("https://cdn.pixabay.com/photo/2015/11/26/00/14/woman-1063100_960_720.jpg"))) 
+                      image:image != null ?FileImage(image):NetworkImage("https://cdn.pixabay.com/photo/2015/11/26/00/14/woman-1063100_960_720.jpg"))) 
               ), //Container
 // (_image != null)?Image.file(_image,fit.BoxFit.fill):Image.network('https://cdn.pixabay.com/photo/2015/11/26/00/14/woman-1063100_960_720.jpg'),
           Positioned(
