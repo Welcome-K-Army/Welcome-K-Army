057fe9c1abfe17335a6d348fbb29e3f7e832a1f6
FlutLab.io <>
2021-05-21 14:02:44 +0000

    Autosave

--- lib/userAnalysisView/dataInputTabBarView/data_input_tab_bar_view.dart
+++ lib/userAnalysisView/dataInputTabBarView/data_input_tab_bar_view.dart
@@ -45,8 +45,8 @@ class DataInputTabBarViewState extends State<DataInputTabBarView> with SingleTic
     Size size = MediaQuery.of(context).size;
     return MultiProvider(
         providers: [
-          ChangeNotifierProvider<TypeStatus>(builder: (_) => TypeStatus()),
-          ChangeNotifierProvider<InstitutionStatus>(builder: (_) => InstitutionStatus()),
+          ChangeNotifierProvider<TypeStatus>(builder: () => TypeStatus()),
+          ChangeNotifierProvider<InstitutionStatus>(builder: () => InstitutionStatus()),
         ],
         child: Scaffold(
             appBar: AppBar(title: Text("UserDataInput"), bottom: TabBar(controller: _tabController, tabs: _tabs)),
