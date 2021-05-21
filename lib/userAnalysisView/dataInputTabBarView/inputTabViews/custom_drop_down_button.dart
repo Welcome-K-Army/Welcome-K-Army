41a69263afe0d45d62ae0410e46b3af04402e95b
FlutLab.io <>
2021-05-21 14:56:13 +0000

    Filtering dropdown view

--- lib/userAnalysisView/dataInputTabBarView/inputTabViews/custom_drop_down_button.dart
+++ lib/userAnalysisView/dataInputTabBarView/inputTabViews/custom_drop_down_button.dart
@@ -56,39 +56,25 @@ class CustomDropDownButtonState extends State<CustomDropDownButton> {
         setState(() {
           dropdownValue = newValue;
         });
- switch(dropdownValue) {
-                  case "사관학교" : 
-                  institutionStatus.setStatus(true);
-                  break;
-                  case "학군단" : 
-                  institutionStatus.setStatus(false);
-                  break;
-                  case "부사관" : 
-                  institutionStatus.setStatus(false);
-                  break;
-                  default : break;
-                }       
+        switch (dropdownValue) {
+          case "사관학교":
+            institutionStatus.setStatus(true);
+            break;
+          case "학군단":
+            institutionStatus.setStatus(false);
+            break;
+          case "부사관":
+            institutionStatus.setStatus(false);
+            break;
+          default:
+            break;
+        }
       },
       items: items.map<DropdownMenuItem<String>>((String value) {
         return DropdownMenuItem<String>(
-            value: value,
-            child: GestureDetector(
-              onTap: () {
-                switch(value) {
-                  case "사관학교" : 
-                  institutionStatus.setStatus(true);
-                  break;
-                  case "학군단" : 
-                  institutionStatus.setStatus(false);
-                  break;
-                  case "부사관" : 
-                  institutionStatus.setStatus(false);
-                  break;
-                  default : break;
-                }
-              },
-              child: Text(value),
-            ));
+          value: value,
+          child: Text(value),
+        );
       }).toList(),
     );
   }
