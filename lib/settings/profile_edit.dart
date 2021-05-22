275f47fe7473cf53687513c06b38927f04f99d90
FlutLab.io <>
2021-05-22 05:46:48 +0000

    Autosave

--- lib/settings/profile_edit.dart
+++ lib/settings/profile_edit.dart
@@ -47,51 +47,29 @@ class _EditProfileState extends State<EditProfile> {
       if (_pickimage != null) {
         image = File(_pickimage.path);
       }
-    });
 
-    // FirebaseStorage _firebaseStorage = FirebaseStorage.instance;
-    // Reference storageReference = await _firebaseStorage.ref().child("profile_image/test.png");
-    // final metadata = SettableMetadata(contentType: 'image/png', customMetadata: {
-    //   'picked-file-path': _pickimage.path
-    // });
-    // UploadTask uploadTask;
-    // // UploadTask storageUploadTask = await storageReference.putFile(await image,metadata);
-
-    // if (kIsWeb) {
-    //   print("web");
-    //   uploadTask = storageReference.putData(await _pickimage.readAsBytes(), metadata);
-    // } else {
-    //   print("no web");
-    //   uploadTask = storageReference.putFile(File(_pickimage.path), metadata);
-    // }
-    // setState(() async {
-    //   downloadURL = await storageReference.getDownloadURL();
-    //   print(downloadURL);
-    // });
-  }
-
-  void uploadfile(File metaimage) async {
     FirebaseStorage _firebaseStorage = FirebaseStorage.instance;
     Reference storageReference = await _firebaseStorage.ref().child("profile_image/test.png");
     final metadata = SettableMetadata(contentType: 'image/png', customMetadata: {
-      'picked-file-path': metaimage.path
+      'picked-file-path': _pickimage.path
     });
     UploadTask uploadTask;
     // UploadTask storageUploadTask = await storageReference.putFile(await image,metadata);
 
     if (kIsWeb) {
       print("web");
-      uploadTask = storageReference.putData(await metaimage.readAsBytes(), metadata);
+      uploadTask = storageReference.putData(await _pickimage.readAsBytes(), metadata);
     } else {
       print("no web");
-      uploadTask = storageReference.putFile(File(metaimage.path), metadata);
+      uploadTask = storageReference.putFile(File(_pickimage.path), metadata);
     }
+    setState(() async {
+      downloadURL = await storageReference.getDownloadURL();
+      print(downloadURL);
+    });
   }
 
-  String getURL() {
-    Reference storageReference = await FirebaseStorage.instance.ref().child("profile_image/test.png");
-    return storageReference.getDownloadURL();
-  }
+
 
   @override
   Widget build(BuildContext context) {
@@ -344,7 +322,7 @@ class _EditProfileState extends State<EditProfile> {
                       onPressed: () {
                         print(getURL());
                         final imageURL = getURL();
-                        uploadfile(image);
+                        
                         userUpdate(profileNameTextEditingController.text, emailTextEditingController.text, genderTextEditingController.text, int.parse(ageTextEditingController.text), imageURL);
                       }, //바뀐 데이터 db로 보내는 함수 만들어야댐 updateUserData
                       //String nickName, String email, String gender, int age
