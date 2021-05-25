522e95319eab625d4347c7a733d0fdd27186a687
FlutLab.io <>
2021-05-25 14:04:35 +0000

    Autosave

--- lib/model/user_data_model.dart
+++ lib/model/user_data_model.dart
@@ -3,7 +3,7 @@ import 'dart:core';
 class UserData {
   String uid;
   String nickName;
-  final String email;
+  String email;
   int age;
   String gender;
   String creationDate;
@@ -19,16 +19,14 @@ class UserData {
     // this.imageURL,
   });
 
-  UserData copy(UserData copydata) {
-    return UserData(
-      uid: copydata.uid,
-      nickName: copydata.nickName,
-      email: copydata.email,
-      gender: copydata.gender,
-      age: copydata.age,
-      creationDate: copydata.creationDate,
-      // copydata.imageURL,
-    );
+  copy(UserData copydata) {
+    this.uid = copydata.uid;
+    this.nickName = copydata.nickName;
+    this.email = copydata.email;
+    this.gender = copydata.gender;
+    this.age = copydata.age;
+    this.creationDate = copydata.creationDate;
+    // copydata.imageURL,
   }
 
   factory UserData.fromJson(Map<String, dynamic> json) {
