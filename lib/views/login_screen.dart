6ff5c2fde1b2d8413a761df9e5126abd8b9ffbb1
FlutLab.io <>
2021-05-26 11:39:47 +0000

    Autosave

--- lib/views/login_screen.dart
+++ lib/views/login_screen.dart
@@ -19,9 +19,7 @@ class _LoginViewState extends State<Login> {
   TextEditingController _emailController = TextEditingController(); //email 컨트롤러
   TextEditingController _passwordController = TextEditingController(); //password 컨트롤러
   UserData luser;
-  void uload() async {
-    luser = await userLoad();
-  }
+
 
   @override
   Widget build(BuildContext context) {
@@ -326,5 +324,8 @@ class _LoginViewState extends State<Login> {
         ),
       ),
     );
+  }
+    void uload() async {
+    luser = await userLoad();
   }
 }
