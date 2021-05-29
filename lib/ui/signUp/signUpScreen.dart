d36d98f2a61971791e40c693bf39513645baaaa8
FlutLab.io <>
2021-05-29 14:35:36 +0000

    Autosave

--- lib/ui/signUp/signUpScreen.dart
+++ lib/ui/signUp/signUpScreen.dart
@@ -51,7 +51,7 @@ class _SignUpState extends State<SignUpScreen> {
           auth.UserCredential result = await auth.FirebaseAuth.instance.createUserWithEmailAndPassword(email: _emailController.text.trim(), password: _passwordController.text.trim());
           if (_image != null) {
             updateProgress('Uploading image, Please wait...');
-            profilePicUrl = await FireStoreUtils().uploadUserImageToFireStorage(_image, result.user.uid);
+            // profilePicUrl = await FireStoreUtils().uploadUserImageToFireStorage(_image, result.user.uid);
           }
           User user = User(email: _emailController.text, nickName: _nicknameController.text, age: _userAge, gender: userGender(), userID: result.user.uid, active: true, profilePictureURL: profilePicUrl);
           await FireStoreUtils.firestore.collection(USERS).doc(result.user.uid).set(user.toJson());
