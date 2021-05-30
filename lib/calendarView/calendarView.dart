b6bf38f1842fabbf1a40ede1045889f174ce62f7
FlutLab.io <>
2021-05-30 11:07:48 +0000

    Autosave

---
+++ lib/calendarView/calendarView.dart
@@ -0,0 +1,86 @@
+import 'package:flutter/material.dart'l
+import 'package:syncfusion_flutter_calendar/calendar.dart';
+
+class CalendarView extends StatefulWidget{
+  CalendarViewState createState() => new CalendarViewState();
+}
+
+class CalendarViewState extends State<CalendarView> {
+  CalendarViewState();
+  
+  final List<String> _subjectCollection = <String>[];
+  final List<Color> _colorCollection = <Color>[];
+  final _MeetingDataSource _events = _MeetingDataSource(<_Meeting>[]);
+  final DateTime _minDate =
+          DateTime.now().subtract(const Duration(days: 365 ~/ 2)),
+      _maxDate = DateTime.now().add(const Duration(days: 365 ~/ 2));
+
+  final List<CalendarView> _allowedViews = <CalendarView>[
+    CalendarView.day,
+    CalendarView.week,
+    CalendarView.workWeek,
+    CalendarView.month,
+    CalendarView.schedule
+  ];
+
+  // final List<String> _viewNavigationModeList =<String>['Snap', 'None'].toList();
+
+  /// Global key used to maintain the state, when we change the parent of the
+  /// widget
+  final GlobalKey _globalKey = GlobalKey();
+  final ScrollController _controller = ScrollController();
+  final CalendarController _calendarController = CalendarController();
+
+  List<DateTime> _blackoutDates = <DateTime>[];
+  bool _showLeadingAndTrailingDates = true;
+  bool _showDatePickerButton = true;
+  bool _allowViewNavigation = true;
+  bool _showCurrentTimeIndicator = true;
+
+  ViewNavigationMode _viewNavigationMode = ViewNavigationMode.snap;
+  String _viewNavigationModeString = 'Snap';
+  
+  @override
+  void initState() {
+    _calendarController.view = CalendarView.month;
+    addAppointmentDetails();
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final Widget calendar = Theme(
+
+        /// The key set here to maintain the state,
+        ///  when we change the parent of the widget
+        key: _globalKey,
+        data: model.themeData.copyWith(accentColor: model.backgroundColor),
+        child: _getGettingStartedCalendar(_calendarController, _events,
+            _onViewChanged, _minDate, _maxDate, scheduleViewBuilder));
+
+    final double screenHeight = MediaQuery.of(context).size.height;
+    return Scaffold(
+      body: Row(children: <Widget>[
+        Expanded(
+          child: _calendarController.view == CalendarView.month &&
+                  model.isWebFullView &&
+                  screenHeight < 800
+              ? Scrollbar(
+                  isAlwaysShown: true,
+                  controller: _controller,
+                  child: ListView(
+                    controller: _controller,
+                    children: <Widget>[
+                      Container(
+                        color: model.cardThemeColor,
+                        height: 600,
+                        child: calendar,
+                      )
+                    ],
+                  ))
+              : Container(color: model.cardThemeColor, child: calendar),
+        )
+      ]),
+    );
+  }
+}
