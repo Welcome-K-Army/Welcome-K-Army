ab140719448b999a693ad3fa6bc8d07ea90c1a36
FlutLab.io <>
2021-06-01 14:46:58 +0000

    Autosave

--- lib/calendarView/customCalendarView.dart
+++ lib/calendarView/customCalendarView.dart
@@ -6,10 +6,25 @@ import 'provider/event_provider.dart';
 
 import 'widget/calendar_widget.dart';
 
-class CustomCalendarView extends StatefulWidget {
-  CustomCalendarViewState createState() => new CustomCalendarViewState();
+class CustomCalendarView extends StatelessWidget {
+  final List<CalendarView> _allowedViews = <CalendarView>[
+    CalendarView.day,
+    CalendarView.week,
+    CalendarView.workWeek,
+    CalendarView.month,
+    CalendarView.schedule
+  ];
+
+  @override
+  Widget build(BuildContext context) => ChangeNotifierProvider<EventProvider>(
+    create: (context) => EventProvider(),
+    child: Scaffold(
+      body: CalendarWidget(),
+    )
+  );
 }
 
+/*
 class CustomCalendarViewState extends State<CustomCalendarView> {
   CustomCalendarViewState();
 
@@ -29,3 +44,4 @@ class CustomCalendarViewState extends State<CustomCalendarView> {
     )
   );
 }
+*/
