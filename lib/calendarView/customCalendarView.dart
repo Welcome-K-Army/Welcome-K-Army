114806e49752fdaf5d98b438a300e801fe6285de
FlutLab.io <>
2021-06-01 14:40:45 +0000

    Autosave

--- lib/calendarView/customCalendarView.dart
+++ lib/calendarView/customCalendarView.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:syncfusion_flutter_calendar/calendar.dart';
-import 'package:syncfusion_flutter_calendar/calendar.dart';
 import 'package:provider/provider.dart';
 
 import 'provider/event_provider.dart';
