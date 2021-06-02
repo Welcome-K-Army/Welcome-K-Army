9e68f3d33f0662f604fcaa539d421b39daa86834
FlutLab.io <>
2021-06-02 14:57:10 +0000

    Autosave

--- lib/calendarView/widget/calendar_widget.dart
+++ lib/calendarView/widget/calendar_widget.dart
@@ -8,10 +8,62 @@ import '../model/event_data_source.dart';
 import '../page/event_editing_page.dart';
 import '../provider/event_provider.dart';
 
-class CalendarWidget extends StatefulWidget {
-  CalendarWidgetState createState() => new CalendarWidgetState();
+class CalendarWidget extends StatelessWidget {
+  final List<CalendarView> _allowedViews = <CalendarView>[
+    CalendarView.day,
+    CalendarView.week,
+    CalendarView.month,
+    CalendarView.schedule
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    final events = Provider.of<EventProvider>(context).events;
+    return Scaffold(
+        appBar: AppBar(title: Text("일정"), actions: <Widget>[
+          PopupMenuButton(
+              icon: Icon(Icons.add),
+              onSelected: (value) {
+                if (value == 1) {
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => EventEditingPage()),
+                      );
+                }
+              },
+              itemBuilder: (context) => [
+                    PopupMenuItem(
+                      value: 1,
+                      child: Text("일정 추가"),
+                    ),
+                    PopupMenuItem(
+                      value: 2,
+                      child: Text("학교 일정 추가"),
+                    )
+                  ])
+        ]),
+        body: SfCalendar(
+            view: CalendarView.month,
+            dataSource: EventDataSource(events),
+            initialSelectedDate: DateTime.now(),
+            cellBorderColor: Colors.transparent,
+            showDatePickerButton: true,
+            allowedViews: _allowedViews,
+            onLongPress: (details) {
+              final provider = Provider.of<EventProvider>(context, listen: false);
+              print(provider);
+              provider.setDate(details.date);
+              showModalBottomSheet(
+                context: context,
+                builder: (context) => TasksWidget(),
+              );
+            }),
+            );
+
 }
 
+/*
 class CalendarWidgetState extends State<CalendarWidget> {
   CalendarWidgetState();
 
@@ -68,3 +120,4 @@ class CalendarWidgetState extends State<CalendarWidget> {
             );
   }
 }
+*/
