e72f5f1cf20f92db5b5828084bdf1c59868e72af
FlutLab.io <>
2021-06-02 03:47:03 +0000

    Autosave

--- lib/calendarView/customCalendarView.dart
+++ lib/calendarView/customCalendarView.dart
@@ -5,14 +5,12 @@ import 'package:provider/provider.dart';
 import 'provider/event_provider.dart';
 
 import 'widget/calendar_widget.dart';
-import 'package:flutter/src/material/app.dart';
 
 class CustomCalendarView extends StatelessWidget {
   @override
   Widget build(BuildContext context) => ChangeNotifierProvider<EventProvider>(
-    create: (context) => EventProvider(),
-    child: MaterialApp(
-      home: CalendarWidget(),
-    )
-  );
+      create: (_) => EventProvider(),
+      child: Scaffold(
+        body: CalendarWidget(),
+      ));
 }
