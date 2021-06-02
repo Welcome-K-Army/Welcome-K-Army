d190046c8bd04df5d2ec4e92722fe2bb702de488
FlutLab.io <>
2021-06-02 14:40:20 +0000

    Autosave

--- lib/calendarView/page/event_editing_page.dart
+++ lib/calendarView/page/event_editing_page.dart
@@ -76,8 +76,8 @@ class _EventEditingPageState extends State<EventEditingPage> {
             primary: Colors.transparent,
             shadowColor: Colors.transparent,
           ),
-          onPressed: () async{
-            saveForm(provider)
+          onPressed: () async {
+            saveForm(provider);
           },
           icon: Icon(Icons.done),
           label: Text('SAVE'),
