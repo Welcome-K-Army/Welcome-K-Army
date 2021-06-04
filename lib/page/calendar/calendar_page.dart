9bc81b96016aa9e32e4291c968b199600f86c805
FlutLab.io <>
2021-06-04 03:41:00 +0000

    Autosave

--- lib/page/calendar/calendar_page.dart
+++ lib/page/calendar/calendar_page.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
-import 'provider/event_provider.dart';
+import '../../provider/event_provider.dart';
 
-import 'widget/calendar_widget.dart';
+import '../../widget/calendar/calendar_widget.dart';
 
 class CalendarPage extends StatelessWidget {
   @override
