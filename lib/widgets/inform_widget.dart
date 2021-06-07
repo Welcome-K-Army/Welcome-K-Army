500bd20d80d6bddca1c36ec45d4106857176b1c5
FlutLab.io <>
2021-06-07 13:12:57 +0000

    Autosave

--- lib/widgets/inform_widget.dart
+++ lib/widgets/inform_widget.dart
@@ -2,34 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:url_launcher/url_launcher.dart';
 
-import '../widgets/inform_widget.dart';
-
-void main() => runApp(MyApp());
-
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      // Application name
-      title: 'Flutter Hello World',
-      // Application theme data, you can set the colors for the application as
-      // you want
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      // A widget which will be started on application startup
-      home: MyHomePage(),
-    );
-  }
+class InformWidget extends StatefulWidget {
+  InformWidgetState createState() => InformWidgetState();
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePageState createState() => MyHomePageState();
-}
-
-class MyHomePageState extends State<MyHomePage> {
-  bool iconDialog = false;
+class InformWidgetState extends State<InformWidget> {
   List<String> informList = [
     "서울특별시 노원구 공릉2동 화랑로 574",
     "http://www.kma.ac.kr/",
@@ -52,53 +29,6 @@ class MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-    return DefaultTabController(
-      length: 4,
-      child: Scaffold(
-        appBar: AppBar(
-          bottom: TabBar(
-            //indicator는 현재 선택된 Tab에 대한 정보, 추후 디자인 수정 요망
-            indicatorSize: TabBarIndicatorSize.label,
-            indicatorColor: Colors.red,
-            indicatorWeight: 4,
-            labelColor: Colors.white,
-            unselectedLabelColor: Colors.black38,
-            labelStyle: TextStyle(
-              fontSize: 14,
-            ),
-            tabs: [
-              Tab(
-                icon: Icon(Icons.home, size: 20),
-                text: 'Home',
-              ),
-              Tab(
-                icon: Icon(Icons.search, size: 20),
-                text: 'Search',
-              ),
-              Tab(
-                icon: Icon(Icons.search, size: 20),
-                text: 'Search',
-              ),
-              Tab(
-                icon: Icon(Icons.people, size: 20),
-                text: 'My',
-              ),
-            ],
-          ),
-        ),
-        body: TabBarView(
-          children: [
-            InformWidget(),
-            Text('경쟁률'),
-            Text('입시정보'),
-            Text('교육과정'),
-          ],
-        ),
-      ),
-    ); // Column
-  }
-
-  Widget buildInform() {
     return Container(
       child: SingleChildScrollView(
         scrollDirection: Axis.vertical,
