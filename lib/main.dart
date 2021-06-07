7db9706f05617ef6344d8e2b4957afbdc81fc214
FlutLab.io <>
2021-06-07 13:16:37 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
-import 'package:url_launcher/url_launcher.dart';
 
-import 'widgets/inform_widget.dart';
+import 'package:helloworld/lib/widgets/inform_widget.dart';
 
 void main() => runApp(MyApp());
 
@@ -96,90 +94,5 @@ class MyHomePageState extends State<MyHomePage> {
         ),
       ),
     ); // Column
-  }
-
-  Widget buildInform() {
-    return Container(
-      child: SingleChildScrollView(
-        scrollDirection: Axis.vertical,
-        child: Column(
-          children: <Widget>[
-            Image.asset("lib/image/1.jpg"),
-            Divider(color: Color(0xFFD6D6D6), thickness: 1),
-            ListTile(title: Text("육군사관학교"), subtitle: Text("육군, 사관학교")),
-            Divider(color: Color(0xFFD6D6D6), thickness: 1),
-            buildAddressTile(),
-            buildUrlTile(),
-            buildPhoneNumberTile(),
-          ],
-        ),
-      ),
-    );
-  }
-
-  Widget buildAddressTile() {
-    return ListTile(
-        leading: Tooltip(
-            message: "주소 복사",
-            child: IconButton(
-                icon: Icon(informIconList[0]),
-                onPressed: () {
-                  Clipboard.setData(ClipboardData(text: informList[0]));
-                })),
-        title: Text(informList[0]),
-        trailing: Tooltip(
-            message: "복사 하기",
-            child: IconButton(
-                icon: Icon(informIconList[3]),
-                onPressed: () {
-                  Clipboard.setData(ClipboardData(text: informList[0]));
-                })));
-  }
-
-  Widget buildUrlTile() {
-    return ListTile(
-        leading: Tooltip(
-            message: "웹사이트 열기",
-            child: IconButton(
-                icon: Icon(informIconList[1]),
-                onPressed: () {
-                  _url_launcher(informList[1]);
-                })),
-        title: Text(informList[1]),
-        trailing: Tooltip(
-            message: "복사 하기",
-            child: IconButton(
-                icon: Icon(informIconList[3]),
-                onPressed: () {
-                  Clipboard.setData(ClipboardData(text: informList[1]));
-                })));
-  }
-
-  Widget buildPhoneNumberTile() {
-    return ListTile(
-        leading: Tooltip(
-            message: "전화 걸기",
-            child: IconButton(
-                icon: Icon(informIconList[2]),
-                onPressed: () {
-                  _url_launcher("tel:$informList[2]");
-                })),
-        title: Text(informList[2]),
-        trailing: Tooltip(
-            message: "복사 하기",
-            child: IconButton(
-                icon: Icon(informIconList[3]),
-                onPressed: () {
-                  Clipboard.setData(ClipboardData(text: informList[2]));
-                })));
-  }
-
-  Future<void> _url_launcher(String url) async {
-    if (await canLaunch(url)) {
-      print("launch");
-      await launch(url);
-    } else {
-      throw 'Could not launch $url';
-    }
   }
 }
