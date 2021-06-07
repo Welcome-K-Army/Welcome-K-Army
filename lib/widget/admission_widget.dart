ad830e6ac4ad8899c95e562a02dc41d21d99d750
FlutLab.io <>
2021-06-07 14:14:09 +0000

    Autosave

--- lib/widget/admission_widget.dart
+++ lib/widget/admission_widget.dart
@@ -7,12 +7,34 @@ class AdmissionWidget extends StatefulWidget {
 }
 
 class AdmissionWidgetState extends State<AdmissionWidget> {
-    @override
+  /*  
+  // Load document from the Asset
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        body: Container(
-            child: SfPdfViewer.network(
-                'http://ebooks.syncfusion.com/downloads/flutter-succinctly/flutter-succinctly.pdf')));
+    return Scaffold(body: Container(child: SfPdfViewer.asset('assets/flutter-succinctly.pdf')));
+  }
+  */
+
+  /*
+  // Load document from the File
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(body: Container(child: SfPdfViewer.file(File('storage/emulated/0/Download/flutter-succinctly.pdf'))));
+  }
+  */
+
+  /*
+  // Load document from the Memory
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(body: Container(child: SfPdfViewer.memory(bytes)));
+  }
+  */
+
+  // Load document from the Network
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return Scaffold(body: Container(height: size.height, width: size.width, child: SfPdfViewer.network('http://ebooks.syncfusion.com/downloads/flutter-succinctly/flutter-succinctly.pdf')));
   }
 }
-  
