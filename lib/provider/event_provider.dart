9e934957a90010ea4a221ec6f9c01b9650c7df4c
FlutLab.io <>
2021-06-12 08:51:39 +0000

    Autosave

--- lib/provider/event_provider.dart
+++ lib/provider/event_provider.dart
@@ -5,7 +5,7 @@ import 'package:Army/model/calendar/event.dart';
 import 'package:Army/services/authenticate.dart';
 
 class EventProvider extends ChangeNotifier {
-  final List<Event> _events = [];
+  final List<Event> _events = getUserCalendarEvent();
 
   List<Event> get events => _events;
 
