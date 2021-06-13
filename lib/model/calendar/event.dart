18f68fbd7838e6b58187c3967807a3c91e8a2d60
FlutLab.io <>
2021-06-13 07:01:49 +0000

    Autosave

--- lib/model/calendar/event.dart
+++ lib/model/calendar/event.dart
@@ -6,11 +6,12 @@ extension HexColor on Color {
     final buffer = StringBuffer();
     if (hexString.length == 6 || hexString.length == 7) buffer.write('ff');
     buffer.write(hexString.replaceFirst('#', '0x'));
+    print(int.parse(buffer.toString(), radix: 16));
     return Color(int.parse(buffer.toString(), radix: 16));
   }
 
   /// Prefixes a hash sign if [leadingHashSign] is set to `true` (default is `true`).
-  String toHex({bool leadingHashSign = true}) => '${leadingHashSign ? '#' : ''}'
+  String toHex({bool leadingHashSign = true}) => '${leadingHashSign ? '# ' : ''}'
       '${alpha.toRadixString(16).padLeft(2, '0')}'
       '${red.toRadixString(16).padLeft(2, '0')}'
       '${green.toRadixString(16).padLeft(2, '0')}'
@@ -35,14 +36,7 @@ class Event {
   });
 
   factory Event.fromJson(Map<String, dynamic> parsedJson) {
-    return new Event(
-      title: parsedJson['title'] ?? '',
-      description: parsedJson['description'] ?? '',
-      from: parsedJson['from'] ?? '',
-      to: parsedJson['to'] ?? '',
-      backgroundColor: HexColor.fromHex(parsedJson['backgroundColor']) ?? Colors.red,
-      isAllDay: parsedJson['isAllDay'] ?? false
-    ); 
+    return new Event(title: parsedJson['title'] ?? '', description: parsedJson['description'] ?? '', from: parsedJson['from'] ?? '', to: parsedJson['to'] ?? '', backgroundColor: HexColor.fromHex(parsedJson['backgroundColor']) ?? Colors.red, isAllDay: parsedJson['isAllDay'] ?? false);
   }
 
   Map<String, dynamic> toJson() {
@@ -66,4 +60,3 @@ class SchoolEvent {
     this.events,
   });
 }
-
