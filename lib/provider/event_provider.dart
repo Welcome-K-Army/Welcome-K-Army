5c041341cb2b84a622d3bc9575d4544b07c1b72d
FlutLab.io <>
2021-06-13 06:25:07 +0000

    Autosave

--- lib/provider/event_provider.dart
+++ lib/provider/event_provider.dart
@@ -17,8 +17,8 @@ class EventProvider extends ChangeNotifier {
 
   List<Event> get eventsOfSelectedDate => _events;
 
-  void readEvent() async {
-    _events = await FireStoreUtils().getUserCalendarEvent();
+  void readEvent() {
+    _events = FireStoreUtils().getUserCalendarEvent();
     notifyListeners();
   }
 
