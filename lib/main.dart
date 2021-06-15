5455546b155e62f9e4b5d78a50fc15c65bf51b81
FlutLab.io <>
2021-06-15 09:52:52 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,10 @@ import 'dart:async';
 
 import 'package:flutter/material.dart';
 import 'mapView/mapScreen/MapSample.dart'; // Api 사용 예시
+import 'package:provider/provider.dart';
 import 'mapView/filterScreen/FilterScreen.dart'; // 학교 정보 필터링 예시
 
-void main() => runApp(MyApp());
+void main() => runApp(ChangeNotifierProvider(create: (context) => Applicationbloc(), child: MyApp()));
 
 class MyApp extends StatelessWidget {
   @override
