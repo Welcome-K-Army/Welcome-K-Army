6e6e4eb9937045b21cdd6cb0c5775f9a0741dfaf
FlutLab.io <>
2021-06-15 13:47:27 +0000

    Autosave

--- lib/mapView/search/search.dart
+++ lib/mapView/search/search.dart
@@ -8,37 +8,33 @@ import "../filterScreen/data.dart";
 //     prelist.add(value)
 //   }
 
-class Applicationbloc with ChangeNotifier{
-
-  List<School> datalist=fillData(); //하드코딩된 값을 리턴
+class Applicationbloc with ChangeNotifier {
+  List<School> datalist = fillData(); //하드코딩된 값을 리턴
 
   List<School> searchResults;
 
-  searchPlaces(String searchTerm) async{
-    searchResults.add(await check_value(searchTerm));//해당 밸류 검색해서 확인하는 함수
+  searchPlaces(String searchTerm) async {
+    searchResults.add(await check_value(searchTerm)); //해당 밸류 검색해서 확인하는 함수
     notifyListeners();
   }
 
-  check_value(String value) async{
-    for (int i=0 ; i<=datalist.length ; i++){
-      if(value==datalist[i].name){
+  check_value(String value) async {
+    for (int i = 0; i <= datalist.length; i++) {
+      if (value == datalist[i].name) {
         return datalist[i];
-      }else{
+      } else {
         return null;
       }
-    }//for
+    } //for
     notifyListeners();
-  }//check_value
+  } //check_value
 
-  selected_value() async{
-    notifyListeners();
-
-  }
-
-  clear_value(){
-    searchResults=null;
+  selected_value() async {
     notifyListeners();
   }
 
-
+  clear_value() {
+    searchResults = null;
+    notifyListeners();
+  }
 }
