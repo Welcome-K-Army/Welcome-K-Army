fba82cdf2e8e147d4b70024cf7b509547ce17dc4
FlutLab.io <>
2021-06-22 13:27:30 +0000

    Autosave

--- lib/widget/admission_widget.dart
+++ lib/widget/admission_widget.dart
@@ -14,7 +14,7 @@ class AdmissionWidgetState extends State<AdmissionWidget> {
   @override
   void initState() {
     List<String> items = [
-      'https://www.kma.ac.kr:461/sites/kma/2022%ED%95%99%EB%85%84%EB%8F%84(82%EA%B8%B0)%EC%9C%A1%EA%B5%B0%EC%82%AC%EA%B4%80%EC%83%9D%EB%8F%84%EB%AA%A8%EC%A7%91%EC%9A%94%EA%B0%95.pdf',
+      'https://firebasestorage.googleapis.com/v0/b/login-project-afa09.appspot.com/o/pdf%2F2022%ED%95%99%EB%85%84%EB%8F%84(82%EA%B8%B0)%EC%9C%A1%EA%B5%B0%EC%82%AC%EA%B4%80%EC%83%9D%EB%8F%84%EB%AA%A8%EC%A7%91%EC%9A%94%EA%B0%95.pdf?alt=media&token=b0d42ed4-0949-4c14-a41f-758bc966762a',
       'https://firebasestorage.googleapis.com/v0/b/login-project-afa09.appspot.com/o/pdf%2F2022%ED%95%99%EB%85%84%EB%8F%84(%EC%A0%9C82%EA%B8%B0)%20%EC%9C%A1%EA%B5%B0%EC%82%AC%EA%B4%80%EC%83%9D%EB%8F%84%20%EC%84%A0%EB%B0%9C%EC%8B%9C%ED%97%98%20%EC%84%B8%EB%B6%80%EC%8B%9C%ED%96%89%EA%B3%84%ED%9A%8D.pdf?alt=media&token=f2d67e3d-d6ef-4ebc-93f5-0b86d225c447'
     ];
     List<String> itemsTitle = [
@@ -29,24 +29,33 @@ class AdmissionWidgetState extends State<AdmissionWidget> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: ListView.builder(
-          padding: const EdgeInsets.all(8),
-          itemCount: pdfItems.itemsTitle.length,
-          itemBuilder: (BuildContext context, int index) {
-            return ListTile(
-              title: Text(pdfItems.itemsTitle[index]),
-                trailing: Wrap(
-                  spacing: 12, // space between two icons
-                  children: <Widget>[
-                    IconButton(
-                        icon: Icon(Icons.search),
-                        onPressed: () {
-                          Navigator.of(context).push(MaterialPageRoute(builder: (BuildContext context) => PdfViewingPage(pdfItem: pdfItems.items[index], title: pdfItems.itemsTitle[index])));
-                        })
-                  ],
-                )
-            );
-          }),
+      body: Card(
+        color: Colors.white,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(15.0),
+        ),
+        elevation: 4,
+        child: Padding(
+          padding: EdgeInsets.all(10),
+          child: ListView.builder(
+              padding: const EdgeInsets.all(8),
+              itemCount: pdfItems.itemsTitle.length,
+              itemBuilder: (BuildContext context, int index) {
+                return ListTile(
+                    title: Text(pdfItems.itemsTitle[index]),
+                    trailing: Wrap(
+                      spacing: 12, // space between two icons
+                      children: <Widget>[
+                        IconButton(
+                            icon: Icon(Icons.search),
+                            onPressed: () {
+                              Navigator.of(context).push(MaterialPageRoute(builder: (BuildContext context) => PdfViewingPage(pdfItem: pdfItems.items[index], title: pdfItems.itemsTitle[index])));
+                            })
+                      ],
+                    ));
+              }),
+        ),
+      ), // Padding
     );
   }
 }
