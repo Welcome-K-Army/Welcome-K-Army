b221be94a27509763b1ac0309a37059729c05156
FlutLab.io <>
2021-06-22 14:36:49 +0000

    Autosave

--- lib/mapView/detailView/widget/ordinal_combo_bar_line_chart_widget.dart
+++ lib/mapView/detailView/widget/ordinal_combo_bar_line_chart_widget.dart
@@ -20,7 +20,7 @@ class OrdinalComboBarLineChartWidget extends StatelessWidget {
     return new OrdinalComboBarLineChartWidget(
       _createSampleData(),
       // Disable animations for image tests.
-      // animate: false,
+      animate: false,
     );
   }
 
@@ -28,7 +28,7 @@ class OrdinalComboBarLineChartWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return new charts.OrdinalComboChart(seriesList,
-        // animate: animate,
+        animate: animate,
         // Configure the default renderer as a bar renderer.
         defaultRenderer: new charts.BarRendererConfig(
             groupingType: charts.BarGroupingType.grouped),
