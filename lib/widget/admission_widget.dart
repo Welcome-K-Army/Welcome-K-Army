c2419ffee886d40d08b3c76104d73879c24ff875
FlutLab.io <>
2021-06-23 14:24:28 +0000

    Autosave

--- lib/widget/admission_widget.dart
+++ lib/widget/admission_widget.dart
@@ -22,7 +22,8 @@ class AdmissionWidgetState extends State<AdmissionWidget> {
 
   ///Get the PDF document as bytes.
   void getPdfBytes(String url) async {
-    _documentBytes.add(await NetworkAssetBundle(Uri.parse(url)).load(url)).buffer.asUint8List();
+    Uint8List _documentByte = (await NetworkAssetBundle(Uri.parse(url)).load(url)).buffer.asUint8List();
+    _documentBytes.add(_documentByte);
   }
 
   @override
