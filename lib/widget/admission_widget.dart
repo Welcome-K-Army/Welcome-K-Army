9d99ab7117ad890d022561ee9eda9a3a38562468
FlutLab.io <>
2021-06-23 13:29:25 +0000

    Autosave

--- lib/widget/admission_widget.dart
+++ lib/widget/admission_widget.dart
@@ -1,6 +1,7 @@
 import 'dart:io';
-import 'package:flutter/material.dart';
+import 'dart:typed_data';
 
+import 'package:flutter/material.dart';
 import 'package:syncfusion_flutter_pdf/pdf.dart';
 import 'package:open_file/open_file.dart';
 import 'package:path_provider/path_provider.dart';
