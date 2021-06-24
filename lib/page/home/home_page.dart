4e14f3ce181fb43a7a760cd4105e22bce8a1284b
FlutLab.io <>
2021-06-24 10:57:36 +0000

    Autosave

--- lib/page/home/home_page.dart
+++ lib/page/home/home_page.dart
@@ -128,41 +128,43 @@ class HomePageState extends State<HomePage> {
   }
 
   Widget buildSlideBanner() {
-    return Card(
-      color: Colors.white,
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(15.0),
-      ),
-      elevation: 4,
-      child: Padding(
-        padding: EdgeInsets.all(10),
-        child: Swiper(
-            autoplay: true,
-            scale: 0.8,
-            viewportFraction: 1,
-            pagination: SwiperPagination(),
-            itemCount: pdfItems.length, //notice imagelist length
-            itemBuilder: (BuildContext context, int index) {
-              return Padding(
-                  padding: EdgeInsets.all(10.0),
-                  child: Card(
-                      child: Column(children: <Widget>[
-                    InkWell(
-                        onTap: () {
-                          Navigator.of(context).push(MaterialPageRoute(builder: (BuildContext context) => PdfViewingWidget(pdfItem: pdfItems[index])));
-                        },
-                        child: Thumbnail(
-                          dataResolver: () async {
-                            return getPdfBytes(pdfItems[index].itemUrl);
-                          },
-                          mimeType: 'application/pdf',
-                          widgetSize: 200.0 - 40.0,
-                        )),
-                    Text(pdfItems[index].title, style: TextStyle(fontWeight: FontWeight.bold)),
-                  ])));
-            }), // Swiper
-      ),
-    );
+    return Container(
+        height: 300,
+        child: Card(
+          color: Colors.white,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(15.0),
+          ),
+          elevation: 4,
+          child: Padding(
+            padding: EdgeInsets.all(10),
+            child: Swiper(
+                autoplay: true,
+                scale: 0.8,
+                viewportFraction: 1,
+                pagination: SwiperPagination(),
+                itemCount: pdfItems.length, //notice imagelist length
+                itemBuilder: (BuildContext context, int index) {
+                  return Padding(
+                      padding: EdgeInsets.all(10.0),
+                      child: Card(
+                          child: Column(children: <Widget>[
+                        InkWell(
+                            onTap: () {
+                              Navigator.of(context).push(MaterialPageRoute(builder: (BuildContext context) => PdfViewingWidget(pdfItem: pdfItems[index])));
+                            },
+                            child: Thumbnail(
+                              dataResolver: () async {
+                                return getPdfBytes(pdfItems[index].itemUrl);
+                              },
+                              mimeType: 'application/pdf',
+                              widgetSize: 200.0 - 40.0,
+                            )),
+                        Text(pdfItems[index].title, style: TextStyle(fontWeight: FontWeight.bold)),
+                      ])));
+                }), // Swiper
+          ),
+        ));
   }
 
   Widget buildMenu() {
