5fc6bf50d23354a507c0e24aba21965d97f3b98c
FlutLab.io <>
2021-06-25 14:04:12 +0000

    Autosave

--- lib/mapView/filterScreen/BottomSheet/SearchBar.dart
+++ lib/mapView/filterScreen/BottomSheet/SearchBar.dart
@@ -43,7 +43,7 @@ class _SearhBarState extends State<SearhBar> {
                   }
                 } //for                          
               });
-            },
+            },),),
             IconButton(
                         icon: Icon(Icons.search),
                         onPressed: () {
@@ -61,24 +61,7 @@ class _SearhBarState extends State<SearhBar> {
                     ),
 
 
-
-            
-                          
-            // , suffixIcon: Icon(Icons.search),onTap: () {
-            //               setState(()async{
-            //                 final result_data=Arguments(data_filtered.name,data_filtered.address,data_filtered.number,data_filtered.web_address,data_filtered.image,data_filtered.pdfurl,data_filtered.web_address_detail,data_filtered.one,
-            //                   data_filtered.two,data_filtered.three,data_filtered.four);
-            //                 final result= await Navigator.push(
-            //                   context,
-            //                   MaterialPageRoute(builder: (context)=>DetailView(arguments:result_data)),
-            //                 );  
-            //                 controller.clear();
-            //                 data_filtered=[];
-            //             });
-            //         };),
-              
-
-          ), //TextFiled
-        )); 
+           //TextFiled
+        ),); 
   }
 }
