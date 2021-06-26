0fbb6803817fdfc3e4172155f2a52dcbb417a01e
FlutLab.io <>
2021-06-26 01:47:23 +0000

    Autosave

--- lib/mapView/filterScreen/BottomSheet/SearchBar.dart
+++ lib/mapView/filterScreen/BottomSheet/SearchBar.dart
@@ -5,60 +5,58 @@ import '../School.dart';
 import '../data.dart';
 import '../../detailView/detail_main.dart';
 
-
 class SearhBar extends StatefulWidget {
   @override
   _SearhBarState createState() => _SearhBarState();
 }
 
 class _SearhBarState extends State<SearhBar> {
-
   TextEditingController controller = TextEditingController();
-  List<School> dataSet=fillData();
-  List<School> data_filtered=[];
+  List<School> dataSet = fillData();
+  List<School> data_filtered = [];
   // String _searchResult;
-  
+
   @override
-  void initState(){
+  void initState() {
     super.initState();
-    data_filtered=dataSet;
+    data_filtered = dataSet;
   }
+
   @override
   Widget build(BuildContext context) {
     return Container(
-        margin: EdgeInsets.all(3),
-        child:Padding(
-          padding: const EdgeInsets.all(4),
-          child: TextField(
-            controller:controller,
-            textCapitalization: TextCapitalization.words,
-            decoration: InputDecoration(hintText: 'Search Location',icon: Icon(Icons.search)),
-            onChanged:(value)async{              
-              setState((){
-                // _searchResult=value;
-                for (int i = 0; i <= dataSet.length; i++) {
-                  if (value == dataSet[i].name) {
-                    data_filtered.add(dataSet[i]);                 
-                        }
-                     } //for                          
-                    });
-                  },
-            onPressed: () {
-                          setState(()async{
-                            final result_data=Arguments(data_filtered.name,data_filtered.address,data_filtered.number,data_filtered.web_address,data_filtered.image,data_filtered.pdfurl,data_filtered.web_address_detail,data_filtered.one,
-                              data_filtered.two,data_filtered.three,data_filtered.four);
-                            final result= await Navigator.push(
-                              context,
-                              MaterialPageRoute(builder: (context)=>DetailView(arguments:result_data)),
-                            );  
-                            controller.clear();
-                            data_filtered=[];
-                        },);
-                      },
-                ),
-            ),
-          );
-
+      margin: EdgeInsets.all(3),
+      child: Padding(
+        padding: const EdgeInsets.all(4),
+        child: TextField(
+          controller: controller,
+          textCapitalization: TextCapitalization.words,
+          decoration: InputDecoration(hintText: 'Search Location', icon: Icon(Icons.search)),
+          onChanged: (value) async {
+            setState(() {
+              // _searchResult=value;
+              for (int i = 0; i <= dataSet.length; i++) {
+                if (value == dataSet[i].name) {
+                  data_filtered.add(dataSet[i]);
+                }
+              } //for
+            });
+          },
+          onPressed: (data_filtered) {
+            setState(
+              () async {
+                final result_data = Arguments(data_filtered.name, data_filtered.address, data_filtered.number, data_filtered.web_address, data_filtered.image, data_filtered.pdfurl, data_filtered.web_address_detail, data_filtered.one, data_filtered.two, data_filtered.three, data_filtered.four);
+                final result = await Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => DetailView(arguments: result_data)),
+                );
+                controller.clear();
+                data_filtered = [];
+              },
+            );
+          },
+        ),
+      ),
+    );
   }
 }
-
