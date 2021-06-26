a22b26964f97b9f799690057cd990a829b2b8ba9
FlutLab.io <>
2021-06-26 01:55:13 +0000

    Autosave

--- lib/mapView/filterScreen/BottomSheet/SearchBar.dart
+++ lib/mapView/filterScreen/BottomSheet/SearchBar.dart
@@ -42,19 +42,19 @@ class _SearhBarState extends State<SearhBar> {
               } //for
             });
           },
-          onTap: () {
-            setState(
-              () async {
-                final result_data = Arguments(data_filtered.name, data_filtered.address, data_filtered.number, data_filtered.web_address, data_filtered.image, data_filtered.pdfurl, data_filtered.web_address_detail, data_filtered.one, data_filtered.two, data_filtered.three, data_filtered.four);
-                final result = await Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => DetailView(arguments: result_data)),
-                );
-                controller.clear();
-                data_filtered = [];
-              },
-            );
-          },
+          // onTap: () {
+          //   setState(()
+          //       async {
+          //       final result_data = Arguments(data_filtered.name, data_filtered.address, data_filtered.number, data_filtered.web_address, data_filtered.image, data_filtered.pdfurl, data_filtered.web_address_detail, data_filtered.one, data_filtered.two, data_filtered.three, data_filtered.four);
+          //       final result = await Navigator.push(
+          //         context,
+          //         MaterialPageRoute(builder: (context) => DetailView(arguments: result_data)),
+          //       );
+          //       // controller.clear();
+          //       data_filtered = [];
+          //     },
+          //   );
+          // },
         ),
       ),
     );
