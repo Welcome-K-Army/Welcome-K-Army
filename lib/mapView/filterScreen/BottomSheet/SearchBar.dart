e1bb2bb807735531e62695348fc8b4b1c1bcad5c
FlutLab.io <>
2021-06-26 02:02:52 +0000

    Autosave

--- lib/mapView/filterScreen/BottomSheet/SearchBar.dart
+++ lib/mapView/filterScreen/BottomSheet/SearchBar.dart
@@ -15,7 +15,7 @@ class _SearhBarState extends State<SearhBar> {
   TextEditingController controller = TextEditingController();
   List<School> dataSet = fillData();
   List<School> data_filtered = [];
-  List<School>result_data=[];
+  List<School> result_data=[];
   // String _searchResult;
 
   @override
@@ -47,10 +47,10 @@ class _SearhBarState extends State<SearhBar> {
           onTap: () {
             setState(()
                 async {
-                final result_data = Arguments(data_filtered.name, data_filtered.address, data_filtered.number, data_filtered.web_address, data_filtered.image, data_filtered.pdfurl, data_filtered.web_address_detail, data_filtered.one, data_filtered.two, data_filtered.three, data_filtered.four);
+                final arguments = Arguments(data_filtered.name, data_filtered.address, data_filtered.number, data_filtered.web_address, data_filtered.image, data_filtered.pdfurl, data_filtered.web_address_detail, data_filtered.one, data_filtered.two, data_filtered.three, data_filtered.four);
                 final result = await Navigator.push(
                   context,
-                  MaterialPageRoute(builder: (context) => DetailView(arguments: result_data)),
+                  MaterialPageRoute(builder: (context) => DetailView(arguments: arguments)),
                 );
                 // controller.clear();
                 data_filtered = [];
