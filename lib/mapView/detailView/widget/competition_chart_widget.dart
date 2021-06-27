df8af166527314c53a77bdb487f3ef4d7e42f1b4
FlutLab.io <>
2021-06-27 03:05:02 +0000

    Autosave

--- lib/mapView/detailView/widget/competition_chart_widget.dart
+++ lib/mapView/detailView/widget/competition_chart_widget.dart
@@ -26,15 +26,15 @@ class CompetitionChartWidgetState extends State<CompetitionChartWidget> {
 
   filter_competiton(current_name) {
     List<List<dynamic>> data;
-    List<List<dynamic>> filter_data = [];
+    List<List<String>> filter_data = [];
 
     data = loadAsset();
     for (int i = 1; i < data.length; i++) {
       if (data[2][i] == current_name) {
         filter_data.add([
-          data[1][i],
-          data[4][i],
-          data[5][i]
+          data[1][i],//name
+          data[4][i],//man
+          data[5][i]//woman
         ]);
       }
     }
