1bc2460cd8b5209aaced534eab7b924997d77dfa
FlutLab.io <>
2021-06-29 13:59:42 +0000

    Autosave

--- lib/mapView/detailView/detail_main.dart
+++ lib/mapView/detailView/detail_main.dart
@@ -113,10 +113,6 @@ class Arguments {
   String image;
   String pdfurl;
   String web_address_detail;
-  List<int> one;
-  List<int> two;
-  List<int> three;
-  List<int>four;
 
-  Arguments(this.name, this.address, this.number, this.web_address, this.image,this.pdfurl,this.web_address_detail,this.one,this.two,this.three,this.four);
+  Arguments(this.name, this.address, this.number, this.web_address, this.image,this.pdfurl,this.web_address_detail);
 }
